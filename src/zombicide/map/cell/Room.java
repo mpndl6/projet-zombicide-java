cdb22a1b9c8601998ab2044889158cad2e74e4af
pc <youssef.chah.etu@univ-lille.fr>
2024-01-24 17:18:41 +0100

    constructeur class Room

--- src/zombicide/map/cell/Room.java
+++ src/zombicide/map/cell/Room.java
@@ -1,4 +1,24 @@
 package zombicide.cell;
 
-public class Room {
+public class Room extends Cell {
+
+    protected List<Item> items;
+    protected Map<Location,Door> doors;
+    protected int width;
+    protected int height;
+
+    public Room(Position p , int w , int h) {
+        super(p);
+        this.width = w;
+        this.height = h;
+        this.items = new ArrayList<Item>();
+        this.doors = new HashMap<Location,Door>();
+
+        List<Location> locations = this.locationOfDoors();
+
+        for(Location l : locations) {
+            this.doors.put(l,new Door(l));
+        }
+    }
+
 }
