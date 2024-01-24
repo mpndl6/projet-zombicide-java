2c2ec636c74d63169d8b562649fa0caf5fd49d79
Gabrielle Maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-01-24 18:39:38 +0100

    ajout du constructeur de la classe street

--- src/zombicide/map/cell/Street.java
+++ src/zombicide/map/cell/Street.java
@@ -1,5 +1,22 @@
-package zombicide.cell;
+package zombicide.map.cell;
+import zombicide.util.Position;
 
+/** the street cell in the map */
 public class Street {
+
+/** streets have or not a wastewater on where the zombies go out*/
+private boolan wastewater;
+
+/** Construct a street
+ * We can set a wastewater or not on the cell
+ */
+public Street(Position p, boolean hasWastewater){
+    super(p);
+    this.wastewater =hasWastewater;
+}
+
+
+
+
 
 }
