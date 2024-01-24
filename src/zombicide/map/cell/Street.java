96d3068cada167fb3d4f9bbad0aa80a6c071cb1f
Gabrielle Maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-01-24 18:43:19 +0100

    ajout de la méthode haswastewater

--- src/zombicide/map/cell/Street.java
+++ src/zombicide/map/cell/Street.java
@@ -9,11 +9,21 @@ private boolan wastewater;
 
 /** Construct a street
  * We can set a wastewater or not on the cell
+ * @param p the position of the cell street
+ * @param hasWastewater is true when the street constains a wastewater
  */
 public Street(Position p, boolean hasWastewater){
     super(p);
-    this.wastewater =hasWastewater;
+    this.wastewater=hasWastewater;
 }
+
+/**
+ * @return true if the cell street constains a wastewater
+ */
+public boolean hasWastewater(){
+    return this.wastewater;
+}
+
 
 
 
