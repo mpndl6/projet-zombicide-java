44bb2a293043e9d6d0d5cc3628b3804858360a70
neillakobako <neillakobako@localhost>
2024-01-24 20:48:57 +0100

    ajout de la méthode addZombies

--- src/zombicide/map/cell/Cell.java
+++ src/zombicide/map/cell/Cell.java
@@ -1,5 +1,21 @@
-package zombicide.cell;
+package zombicide.map.cell;
+
 
 public abstract class Cell {
+	protected List<Survivors> survivors=new ArrayList<Survivors>();
+	protected List<Zombies> zombies=new ArrayList<Zombies>();
+	protected int NoiseLevel;
+	protected position Position;
+	
+	public Cell(int NoiseLevel,position Position) {
+		
+		this.NoiseLevel=NoiseLevel;
+		this.Position=Position;
+	}
+	
+	
+	
+	
+	
 
 }
