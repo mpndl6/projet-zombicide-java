0f63c56348155903ea01ad926f638c6714e67795
Gabrielle Maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-01-25 13:22:03 +0100

    ajout du constructeur et des attributs de la classe map

--- src/zombicide/map/Map.java
+++ src/zombicide/map/Map.java
@@ -1,5 +1,21 @@
 package zombicide.map;
 
+/** class of the board of the game */
 public class Map {
+
+
+protected int heitgh; // heitgh  of Map
+protected int width; // witdh of map
+
+/** Construct a map 
+ * The heeitgh and width are known at creation
+ */
+public Map(int w, int h){
+    this.heitgh = h;
+    this.width = w;
+    initMap();
+}
+
+
 
 }
