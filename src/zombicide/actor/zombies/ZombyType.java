c4a399c3781b5932166e3c8ab1c70eb32854f473
Gabrielle Maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-01-26 19:46:14 +0100

    ajout des méthodes de type pour un Zombie

--- src/zombicide/actor/zombies/ZombyType.java
+++ src/zombicide/actor/zombies/ZombyType.java
@@ -4,5 +4,42 @@ import java.util.Random;
 
 public enum ZombyType {
 
+ WALKER,POWERFUL,ABOMINATION, RUNNER;
+
+
+private static Random ALEA = new Random();
+
+/**
+* return a randomly chosen ZombieType
+* @return a randomly chosen ZombieType
+*/
+public static ZombyType random() {
+    return ZombyType.values() [ ALEA.nextInt(ZombyType.values().length) ];
+}
+
+ /**
+ * @return the zombie of the type in parameter
+ * @param z the zombietype we want to return
+ */
+public static Zombie createZombie(ZombyType z){
+
+    switch (z){
+        case WALKER:
+            return new Walker(); 
+        
+        case RUNNER:
+            return new Runner();
+        
+        case POWERFUL:
+            return new PowerfulZombie();
+        
+        case ABOMINATION:
+            return new Abomination();
+
+        default:
+            return new Zombie();
+    }
+}
+
 
 }
