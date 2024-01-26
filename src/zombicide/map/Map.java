dcc618d6bcd26bafcfc381e2274ac6b2595fb8de
Gabrielle Maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-01-26 20:41:28 +0100

    methode iniMap() et ses complementaires

--- src/zombicide/map/Map.java
+++ src/zombicide/map/Map.java
@@ -24,16 +24,16 @@ public Map (int w, int h){
     initmap();
 }
 
-/*
- * init the map at creation
- * 
+
+/**
+ * This method returns a int between two numbers
+ * @param n first int
+ * @param end second int 
+ * @return an int between n and m
  */
-private void initmap(){
-    int start = 0;
-    int end = this.width;
-    this.divideMapVertically(start, end);
-    end = this.heitgh;
-    divideHorizontally(start, end);
+public int randomIntBetween(int n, int m){
+    Random random = new Random();
+    return random.nextInt(n - m) + n;
 }
 
 /**
@@ -43,57 +43,88 @@ private void initmap(){
  * @param end the end of the section we want to divide
  * @return value of the end of the smallest section of Map
  */
-protected int divideMapVRecursive (int start, int end){
+protected int divideMapVRecursive (int start, int end, int lentgh){
     int w = end - start;
     int lowerBound = 2;
     int higherBound = w - 2;
+    Random random = new Random();
     if (w < 4) {
         for (int i = start; i < end; i++) {
-            for (int j = 0; j < this.height; j++) {
+            for (int j = 0; j < w; j++) {
                 this.cells[i][j] = new Room();
             }
         } // fill cells of room
     } 
     else {
-        int place = random.nextInt(higherBound - lowerBound) + lowerBound; // random between higherBound and lowerBound 
+        int place = randomIntBetween(lowerBound, higherBound); // random between higherBound and lowerBound 
         end = place;
-        for (int j=0; j<this.heitgh; j++){
+        for (int j=0; j<lentgh; j++){
             this.cells[place][j] = new Street();
         }
-        this.divideMapVRecursive (start,end);
+        this.divideMapVRecursive (start,end,lentgh);
         }
     return end;
-    
 }
+
 /**
- * This methode divide Map in Room with Street in between
- * @param start start of Map 
- * @param end end of Map
+ * Initialise principal road
  */
-private void divideMapVertically(int start, int end) {
-    int maximum = this.width - 4;
-    end = this.width;
-    while (start < maximum) {
-        start = this.divideMapVRecursive (start,end);
+public void initPrincipalStreet(){
+    int randomW = this.randomIntBetween(0,this.width);
+    int randomH = this.randomIntBetween(0, this.heitgh);
+
+    for (int i=0; i<this.width;i++){
+        this.cells[i][randomH] = new Street();
+    }
+    for (int i=0; i<this.heitgh;i++){
+        this.cells[i][randomW] = new Street();
     }
 
-/** class of the board of the game */
-public class Map {
+int XcrossRoad = (randomW + this.width-1)/2;
+int YcrossRoad = (randomH + this.heitgh-1)/2;
 
 
-protected int heitgh; // heitgh  of Map
-protected int width; // witdh of map
+this.cells[XcrossRoad][YcrossRoad] = new CrossRoad();
+this.cells[randomW] = new StreetWithWastewater();
+this.cells[randomH] = new StreetWithWastewater();
+this.cells[this.width - randomW] = new StreetWithWastewater();
+this.cells[this.heitgh - randomH] = new StreetWithWastewater();
 
-/** Construct a map 
- * The heeitgh and width are known at creation
+}
+
+
+/*
+ * initialize the map at creation
+ * 
  */
-public Map(int w, int h){
-    this.heitgh = h;
-    this.width = w;
-    initMap();
+private void initmap(){
+    int randomW = this.randomIntBetween(0,this.width);
+    int randomH = this.randomIntBetween(0, this.heitgh);
 
-}
-}
+    for (int i=0; i<this.width;i++){
+        this.cells[i][randomH] = new Street();
+    }
+    for (int i=0; i<this.heitgh;i++){
+        this.cells[i][randomW] = new Street();
+    }
+
+int XcrossRoad = (randomW + this.width-1)/2;
+int YcrossRoad = (randomH + this.heitgh-1)/2;
+
+
+this.cells[XcrossRoad][YcrossRoad] = new CrossRoad();
+this.cells[randomW] = new StreetWithWastewater();
+this.cells[randomH] = new StreetWithWastewater();
+this.cells[this.width - randomW] = new StreetWithWastewater();
+this.cells[this.heitgh - randomH] = new StreetWithWastewater();
+
+
+
+divideMapVRecursive(0, randomW, this.width); // 0 to Principal Street (witdh)
+divideMapVRecursive(randomW+1, this.width, this.width); // Principal Street to end of Map (width)
+divideMapVRecursive(randomH+1, this.heitgh, this.heitgh); // Principal Street to end of Map (heigh)
+divideMapVRecursive(0, randomH, this.heitgh); // 0 to Principal Street (heigh)
+
 }
 
 }
