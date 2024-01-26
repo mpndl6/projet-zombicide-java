ab55e1e88c9dcea1df51345d47649dbcd7cde4e8
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-01-26 20:57:53 +0100

    Ajout classe Position(Constructeur+attributs)

---
+++ src/zombicide/map/util/Position.java
@@ -0,0 +1,24 @@
+package zombicide.map.util;
+
+/**
+* a position in a discrete 2D environment, a position is determined by integer
+* coordinates x and y
+*
+*/
+public class Position {
+
+private int x;
+private int y;
+
+/**
+* 
+*creates a position corresponding to given coordinates
+* 
+* @param x the x coordinate
+* @param y the y coordinate
+*/
+public Position(int x, int y) {
+	this.x = x;
+	this.y = y;
+}
+}
