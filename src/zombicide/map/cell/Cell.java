4e1441c2da7445e77f5f725fe273e04dba465845
Annie Neilla <annieneilla.kobako.etu>
2024-01-28 03:08:03 +0100

    ajout de la javadoc et  changement d'une méthode en public abstract

--- src/zombicide/map/cell/Cell.java
+++ src/zombicide/map/cell/Cell.java
@@ -1,8 +1,12 @@
 package zombicide.map.cell;
-import java.util.*;
-import zombicide.map.util.*;
-import zombicide.actor.survivors.*;
-import zombicide.actor.zombies.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.Position;
+
+import zombicide.actor.survivors.Survivor;
+import zombicide.actor.zombies.Zombie;
 
 public abstract class Cell {
 	
@@ -15,65 +19,101 @@ public abstract class Cell {
 	protected Position position;
 	
 	// Constructor to initialize the cell with noise level and position
-	public Cell(Position position) {
+	public Cell(int noiseLevel,Position position) {
 		
-		this.noiseLevel=0;
+		this.noiseLevel=noiseLevel;
 		this.position=position;
 		this.survivors=new ArrayList<Survivor>();
 		this.zombies=new ArrayList<Zombie>();
 	}
 	
-	// Method to add a zombie to the cell
+	  /**
+     * Adds a zombie to the cell
+     *@param Zombie to be added to the cell
+     */
 	public void addZombies(Zombie z) {
 		this.zombies.add(z);
 	}
 	
-	// Method to add a survivor to the cell
+	  /**
+     * Adds a survivor to the cell
+     *@param Survivor to be added to the cell
+     */
 	public void addSurvivors(Survivor s) {
 		this.survivors.add(s);
 	}
 	
-	// Method to remove a zombie from the cell
+	  /**
+     *  Removes a zombie from the cell
+     *@param Zombie to be removed from the cell
+     */
 	public void removeZombie(Zombie z) {
 		this.zombies.remove(z);
 	}
 	
-	// Method to remove a survivor from the cell
+	  /**
+     *  Removes a survivor from the cell
+     *@param Survivor to be removed from the cell
+     */
 	public void removeSurvivor(Survivor s) {
 		this.survivors.remove(s);
 	}
 	
-	// Method to get the count of survivors in the cell
+	 /**
+     *  Gets the count of survivors in the cell
+     *@return The number of survivors in the cell
+     */
 	public int howManySurvivors() {
 		return this.survivors.size();
 	}
 	
-	// Method to get the count of zombies in the cell
+	 /**
+     *  Gets the count of zombies in the cell
+     *@return The number of zombies in the cell
+     */
 	public int howManyZombies() {
 		return this.zombies.size();
 	}
 	
-	// Method to get the list of survivors in the cell
+	 /**
+     *  
+     *  Gets the list of survivors in the cell
+     *@return List of survivors in the cell
+     */
 	public List<Survivor> getSurvivors(){
 		return this.survivors;
 	}
 	
-	// Method to get the list of zombies in the cell
+	/**
+     *  
+     *  Gets the list of zombies in the cell     
+     * @return List of zombies in the cell
+     */
 	public List<Zombie> getZombies(){
 		return this.zombies;
 	}
 	
-	// Method to get the npise level in the cell
+	/**
+     *  
+     *  Gets the noise level in the cell     
+     * @return The noise level of the cell
+     */
 	public int getNoiseLevel() {
 		return this.noiseLevel;
 	}
 	
-	// Method to check if there are survivors in the cell who can fight
-	public boolean canFight() {
-		return !survivors.isEmpty();
-	}
+	/**
+     *  
+     *  Checks if there are survivors in the cell who can fight    
+     * @return true if there are survivors who can fight, false otherwise
+     */ 
+	public abstract boolean canFight();
 	
-	// Method to get the position of the cell
+	/**
+     *  
+     *  Gets the position of the cell
+     * @return The position of the cell
+     */ 
 	public Position getPosition() {
 		return this.position;
 	}
