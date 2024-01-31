c9400067125c56035c479c8a741b4b2647e1efc9
Chah Youssef <youssef.chah.etu>
2024-01-31 11:22:49 +0100

    methode display

--- src/zombicide/map/Map.java
+++ src/zombicide/map/Map.java
@@ -149,4 +149,35 @@ this.divideMapVertically(randomH+1,this.heigth); // Principal Street to end of M
 }
 */
 
+public void display() {
+    for(int i = 0 , i < this.cells.length , i++) {
+        for(int j = 0 , i < this.cells[0].length , j++) {
+            if(this.cells[i][j] instanceof Room)
+                if(j < this.cells[0].length - 1)
+                    System.out.print(" R  ")
+                else
+                    System.out.println(" R  ")
+            if(this.cells[i][j] instanceof Street)
+                if(j < this.cells[0].length - 1)
+                    System.out.print(" S  ")
+                else
+                    System.out.println(" S  ")
+            if(this.cells[i][j] instanceof Continental)
+                if(j < this.cells[0].length - 1)
+                    System.out.print(" RC ")
+                else
+                    System.out.println(" RC ")
+            if(this.cells[i][j] instanceof StreetWW)
+                if(j < this.cells[0].length - 1)
+                    System.out.print(" SW ")
+                else
+                    System.out.println(" SW ")
+            if(this.cells[i][j] instanceof DrugStore)
+                if(j < this.cells[0].length - 1)
+                    System.out.print(" RD ")
+                else
+                    System.out.println(" RD ")
+        }
+    }
+}
 }
