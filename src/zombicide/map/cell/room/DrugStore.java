0bc0ca52bd999f0accfe3a60ab9c9b2610ca21e9
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-02-02 18:30:46 +0100

    surcharge de la méhode welcomeSurvivor et javadoc

--- src/zombicide/map/cell/room/DrugStore.java
+++ src/zombicide/map/cell/room/DrugStore.java
@@ -4,15 +4,26 @@ import zombicide.map.cell.*;
 import zombicide.map.util.*;
 
 public class DrugStore extends Room {
+
+    /**
+     * It creates the DrugStore of the Map
+     * @param p position of the drugStore
+     * @param w width of Map
+     * @param h heigth of Map
+     */
     public DrugStore(Position p, int w,int h){
         super(p,w,h);
     }
-    
+
     /**
-     * @return true if it's possible to fight in this area
+     * Adds a survivor to the cell
+     * @param Survivor to be added to the cell
+     * When a wurvivor enters into the Drugstore, its icrease the number of vials
      */
-    public boolean canFight(){
-        return false;
+    @Override
+    public void welcomeSurvivor(Survivor s){
+        super.addSurvivors(s);
+        //super.items.add(new Vial());
     }
 
 
