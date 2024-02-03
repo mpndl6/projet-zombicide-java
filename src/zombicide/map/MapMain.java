22bac281d68f11def3f5c155d8275e80e438e052
pc <youssef.chah.etu@univ-lille.fr>
2024-02-03 12:43:26 +0100

    class MapMain

---
+++ src/zombicide/map/MapMain.java
@@ -0,0 +1,15 @@
+package zombicide.map;
+
+import java.util.*;
+
+import zombicide.map.cell.*;
+import zombicide.map.cell.room.*;
+import zombicide.map.util.*;
+
+
+public class MapMain {
+    public static void main(String[] args) {
+        Map map = new Map(5 , 20);
+        map.display();
+    } 
+} 
