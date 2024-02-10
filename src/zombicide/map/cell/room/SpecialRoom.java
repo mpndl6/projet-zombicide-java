b13a47c29250b2f2a4a586493e14f641f28a46fc
youssef.chah.etu <youssef.chah.etu>
2024-02-10 15:15:01 +0100

    abstract class specialRoom

--- src/zombicide/map/cell/room/SpecialRoom.java
+++ src/zombicide/map/cell/room/SpecialRoom.java
@@ -1,5 +1,12 @@
 package zombicide.map.cell.room;
 
-public class SpecialRoom extends Room {
+import zombicide.map.cell.util.*;
+
+public abstract class SpecialRoom extends Room {
+
+    public SpecialRoom(Position p , int w , int h){
+        super(p , w , h);
+        this.cellType = CellType.SPECIALROOM;
+    }  
     
 }
