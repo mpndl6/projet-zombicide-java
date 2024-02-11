7f2ad11e51979759d299c9086b8f140ddf9fdc4b
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-02-11 19:09:58 +0100

    suppression methode display() reporter dans grid

--- src/zombicide/map/Map.java
+++ src/zombicide/map/Map.java
@@ -201,37 +201,5 @@ public class Map {
     }
 
 
-    public void display() {
-        for (int i = 0; i < this.cells.length; i++) {
-            for (int j = 0; j < this.cells[0].length; j++) {
-                if (this.cells[i][j] instanceof Room)
-                    if (j < this.cells[0].length - 1)
-                        System.out.print("R");
-                    else
-                        System.out.println("R");
-                if (this.cells[i][j] instanceof Street)
-                    if (j < this.cells[0].length - 1)
-                        System.out.print(".");
-                    else
-                        System.out.println(".");
-                if (this.cells[i][j] instanceof Continental)
-                    if (j < this.cells[0].length - 1)
-                        System.out.print("*");
-                    else
-                        System.out.println("*");
-                //if(this.cells[i][j] instanceof StreetWW)
-                //if(j < this.cells[0].length - 1)
-                //System.out.print(" SW ");
-                //else
-                //System.out.println(" SW ");
-                if (this.cells[i][j] instanceof DrugStore)
-                    if (j < this.cells[0].length - 1)
-                        System.out.print("+");
-                    else
-                        System.out.println("+");
-            }
-        }
-    }
-
 
 }
