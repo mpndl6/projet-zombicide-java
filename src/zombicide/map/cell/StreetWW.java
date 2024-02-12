52458f608c71d15856dcdd9de6dbedea8a893af5
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-02-12 21:14:14 +0100

    accolade ajout

--- src/zombicide/map/cell/StreetWW.java
+++ src/zombicide/map/cell/StreetWW.java
@@ -2,13 +2,14 @@ package zombicide.map.cell;
 
 import zombicide.map.util.Position;
 
-public class StreetWW extends Street{
+public class StreetWW extends Street {
 
     public StreetWW(Position p) {
         super(p);
     }
 
-    public String toString(){
+    public String toString() {
         return " W ";
 
+    }
 }
