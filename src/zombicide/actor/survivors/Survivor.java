f6170c8a3c4340251c60c7340cd10cc179ecb2f6
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-02-25 17:36:37 +0100

    Ajout du constructeur

--- src/zombicide/actor/survivors/Survivor.java
+++ src/zombicide/actor/survivors/Survivor.java
@@ -1,15 +1,49 @@
 package zombicide.actor.survivors;
-
 import zombicide.actor.Actor;
+import zombicide.item.*;
+import java.util.*;
+import zombicide.item.weapons.*;
 
+/**
+ * class of survivors
+ */
 public class Survivor extends Actor {
 
-    public Survivor(){
+/**
+ * All three level a survivor can reach
+  */
+protected static final int THIRD_LEVEL = 3;
+protected static final int ELEVENTH_LEVEL = 11;
+protected static final int SEVENTH_LEVEL = 7;
+protected static final int STARTING_LEVEL =0;
+protected int currentLevel;
+protected String nickName; // nick
+protected int lifePoint; // life points of the survivor
+protected int XP; // level of expertise of a survivor
+protected Item inHand; // the item the survivor has in hand
+protected List<Item> backPack;
+// protected List<Role> roles;
+protected int actionPoint;
 
-    }
+/**
+ *  Contruct a survivor with their name in parameter.
+ *  At creation, a survivor has a Pistol in hand
+ * 5 life points, 0 XP, 0 action points et nothing in backpack
+ * @param name the nickname of the survivor
+ *
+ */
+public Survivor( String name){
+    this.nickName = name;
+    this.currentLevel = STARTING_LEVEL;
+    this.XP = 0;
+    this.actionPoint = 0;
+    this.backPack = new ArrayList<>();
+    this.inHand = new Pistol();
+}
     
 public int getXP(){
-    return 0;
+    return this.XP;
 }
+
 
 }
