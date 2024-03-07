48f50152a94d7de05cdb6cd38441ac8b99529894
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-03-07 18:12:32 +0100

    Sous classe survivor Fighter avec méthode diceThrows adapté si il attaque un zombie(Il faudrait une méthode IsAttackingZombie()

---
+++ src/zombicide/actor/survivors/type/Fighter.java
@@ -0,0 +1,31 @@
+package zombicide.actor.survivors.type;
+
+import zombicide.actor.survivors.Survivor;
+
+public class Fighter extends Survivor {
+    /**
+     * Contruct a survivor with their name in parameter.
+     * At creation, a survivor has a Pistol in hand
+     * 5 life points, 0 XP, 0 action points et nothing in backpack
+     *
+     * @param name the nickname of the survivor
+     */
+    public Fighter(String name) {
+        super(name);
+    }
+
+    /**
+     * The fighter survivor adds 1 to each dice roll when attacking a zombie.
+     * @param howMany The number of dice to throw.
+     * @return The total result of the dice throws.
+     */
+    public int diceThrows(int howMany) {
+
+        //if (super.isAttackingZombie()) {
+          //  return super.diceThrows(howMany) + 1;
+        //} else {
+            return super.diceThrows(howMany);
+        //}
+        }
+}
+
