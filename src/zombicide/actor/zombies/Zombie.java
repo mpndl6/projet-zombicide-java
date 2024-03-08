83621f65c8180460000638594a62773dab33f7eb
Annie Neilla <annieneilla.kobako.etu>
2024-03-08 01:13:01 +0100

    ajout des méthodes

--- src/zombicide/actor/zombies/Zombie.java
+++ src/zombicide/actor/zombies/Zombie.java
@@ -2,6 +2,18 @@ package zombicide.actor.zombies;
 
 import zombicide.actor.Actor;
 
-public class Zombie extends Actor {
-    
+public abstract class Zombie extends Actor {
+    protected static final int DAMAGES=0;
+     public int lifePoint;
+
+     public Zombie (int lp){
+         super();
+         this.lifePoint=lp;
+     }
+
+     public int howManyDamages(){
+         return DAMAGES;
+     }
+
+     public abstract void takeDamage();
 }
