a0ef1341cb4ff280a923861291699023f2fcd321
Annie Neilla <annieneilla.kobako.etu>
2024-03-08 01:13:35 +0100

    ajout des méthodes

--- src/zombicide/actor/zombies/type/Powerful.java
+++ src/zombicide/actor/zombies/type/Powerful.java
@@ -3,5 +3,19 @@ package zombicide.actor.zombies.type;
 import zombicide.actor.zombies.Zombie;
 
 public class Powerful extends Zombie {
-    
+    public static final int POWERFUL_DAMAGES=3;
+
+    public Powerful(){
+        super(POWERFUL_DAMAGES);
+    }
+
+    public void takeDamage(){
+        lifePoint-=POWERFUL_DAMAGES;
+        if(lifePoint<=0){
+            System.out.println("Le Powerful a été vaincu!");
+        }else{
+            System.out.println("Le Powerful a subi des dégâts. Points de vie restants:"+lifePoint);
+
+        }
+    }
 }
