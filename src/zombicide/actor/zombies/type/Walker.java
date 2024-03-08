c534e8859c02a4837f96c9a29d0d566c7ee5686a
Annie Neilla <annieneilla.kobako.etu>
2024-03-08 01:13:21 +0100

    ajout des méthodes

--- src/zombicide/actor/zombies/type/Walker.java
+++ src/zombicide/actor/zombies/type/Walker.java
@@ -3,5 +3,19 @@ package zombicide.actor.zombies.type;
 import zombicide.actor.zombies.Zombie;
 
 public class Walker extends Zombie{
+    public static final int WALKER_DAMAGES=1;
+    public Walker(){
+        super(WALKER_DAMAGES);
+    }
+
+    public void takeDamage(){
+        lifePoint-=WALKER_DAMAGES;
+        if(lifePoint<=0){
+            System.out.println("Le Walker a été vaincu!");
+        }else{
+            System.out.println("Le Walker a subi des dégâts. Points de vie restants:"+lifePoint);
+
+        }
+    }
     
 }
