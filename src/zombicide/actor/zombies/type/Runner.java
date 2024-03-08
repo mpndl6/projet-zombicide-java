c05af2dec7ab6a0bd553fba7034a9e2ed9498db1
Annie Neilla <annieneilla.kobako.etu>
2024-03-08 01:13:32 +0100

    ajout des méthodes

--- src/zombicide/actor/zombies/type/Runner.java
+++ src/zombicide/actor/zombies/type/Runner.java
@@ -3,5 +3,18 @@ package zombicide.actor.zombies.type;
 import zombicide.actor.zombies.Zombie;
 
 public class Runner extends Zombie{
-    
+    public static final int RUNNER_DAMAGES=1;
+    public Runner(){
+        super(RUNNER_DAMAGES);
+    }
+
+    public void takeDamage(){
+        lifePoint-=RUNNER_DAMAGES;
+        if(lifePoint<=0){
+            System.out.println("Le Runner a été vaincu!");
+        }else{
+            System.out.println("Le Runner a subi des dégâts. Points de vie restants:"+lifePoint);
+
+        }
+    }
 }
