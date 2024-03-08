6d7dfdb3c40c355ff784641549cb686674670ea6
Annie Neilla <annieneilla.kobako.etu>
2024-03-08 01:15:53 +0100

    ajout des méthodes

--- src/zombicide/actor/zombies/type/Abomination.java
+++ src/zombicide/actor/zombies/type/Abomination.java
@@ -3,5 +3,19 @@ package zombicide.actor.zombies.type;
 import zombicide.actor.zombies.Zombie;
 
 public class Abomination extends Zombie{
-    
+    public static final int ABOMINATION_DAMAGES=2;
+
+    public Abomination(){
+        super(ABOMINATION_DAMAGES);
+    }
+
+    public void takeDamage(){
+        lifePoint-=ABOMINATION_DAMAGES;
+        if(lifePoint<=0){
+            System.out.println("L'abomination a été vaincu!");
+        }else{
+            System.out.println("L'abomination a subi des dégâts. Points de vie restants:"+lifePoint);
+
+        }
+    }
 }
