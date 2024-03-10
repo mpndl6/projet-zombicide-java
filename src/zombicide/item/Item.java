a2b9266a9d74bbce4f8aa4e13550d5ab480bedf2
youssef <@youssef.chah.etu>
2024-03-10 21:56:02 +0100

    item:addsurvivor

--- src/zombicide/item/Item.java
+++ src/zombicide/item/Item.java
@@ -3,9 +3,17 @@ package zombicide.item;
 import zombicide.actor.survivors.Survivor;
 
 public abstract class Item {
-    protected Survivor s;
+    protected Survivor survivor;
 
     public Item(){
-
     }
+
+    /**
+     * Adds a survivor to this item.
+     * @param s The survivor to add.
+     */
+    public void addSurvivor(Survivor s) {
+        this.survivor = s;
+    }
+
 }
