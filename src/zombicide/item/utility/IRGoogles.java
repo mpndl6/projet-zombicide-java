eb80ea5bb55b487578644538653baa183aa1e19e
youssef <@youssef.chah.etu>
2024-03-10 22:02:03 +0100

    IRGoogles:use

--- src/zombicide/item/utility/IRGoogles.java
+++ src/zombicide/item/utility/IRGoogles.java
@@ -1,4 +1,28 @@
 package zombicide.item.utility;
 
-public class IRGoogles {
+import zombicide.map.Map;
+import zombicide.map.util.Position;
+
+public class IRGoogles extends Utility {
+    protected Map map;
+
+    public IRGoogles(Map m){
+        this.map = m;
+    }
+
+    /**
+     * Uses the IR goggles utility item to view adjacent map cells.
+     * This method displays information about adjacent cells relative to the survivor's current position.
+     */
+    public void use(){
+        Position p = this.survivor.getCell().getPosition();
+        if(p.getX() + 1 < this.map.getWidth())
+            System.out.printf(this.map.getCells()[p.getX() + 1][p.getY()].toString());
+        if(p.getX() - 1 >= 0)
+            System.out.printf(this.map.getCells()[p.getX() - 1][p.getY()].toString());
+        if(p.getY() + 1 < this.map.getHeight())
+            System.out.printf(this.map.getCells()[p.getX()][p.getY() + 1].toString());
+        if(p.getY() - 1 >= 0)
+            System.out.printf(this.map.getCells()[p.getX()][p.getY() - 1].toString());
+    }
 }
