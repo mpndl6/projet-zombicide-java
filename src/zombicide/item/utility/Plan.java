709c1cf0c5625ed504444da5625356e07ae10454
youssef <@youssef.chah.etu>
2024-03-10 21:57:44 +0100

    plan:use

--- src/zombicide/item/utility/Plan.java
+++ src/zombicide/item/utility/Plan.java
@@ -1,8 +1,21 @@
 package zombicide.item.utility;
+import grid.Grid;
+import zombicide.map.*;
 
 public class Plan extends Utility {
+    protected Map map;
 
-    public Plan(){
+    public Plan(Map m){
+        this.map = m;
+    }
 
+    /**
+     * Uses the plan utility item to display the associated map.
+     * This method overrides the use method from the Utility class.
+     */
+    @Override
+    public void use() {
+        Grid grid= new Grid(this.map, 5);
+        grid.displayGrid();
     }
 }
