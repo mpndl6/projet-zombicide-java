f05969f1479d712a6fa47c75f7fe7a71949eae6a
youssef <@youssef.chah.etu>
2024-03-10 21:56:45 +0100

    vial:use

--- src/zombicide/item/utility/Vial.java
+++ src/zombicide/item/utility/Vial.java
@@ -6,4 +6,13 @@ public class Vial extends Utility {
     public Vial(){
 
     }
+
+    /**
+     * Uses the vial utility item to increase the life points of the survivor.
+     * This method overrides the use method from the Utility class.
+     */
+    @Override
+    public void use() {
+        this.survivor.increaseLifePoints();
+    }
 }
