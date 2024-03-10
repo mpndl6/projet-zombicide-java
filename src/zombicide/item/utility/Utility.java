8ad3d6f80a19783bdc7eaefb1aba083fcc645548
youssef <@youssef.chah.etu>
2024-03-10 21:52:54 +0100

    Utility :use

--- src/zombicide/item/utility/Utility.java
+++ src/zombicide/item/utility/Utility.java
@@ -2,8 +2,13 @@ package zombicide.item.utility;
 
 import zombicide.item.Item;
 
-public class Utility extends Item {
+public abstract class Utility extends Item {
     public Utility(){
-
     }
+
+    /**
+     * Defines the action of using the utility item.
+     * Subclasses should override this method to specify the behavior of the specific utility item.
+     */
+    public void use() {}
 }
