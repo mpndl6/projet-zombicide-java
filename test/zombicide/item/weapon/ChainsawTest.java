cd11bc2f6bca0612c5735f75d7070e6eaa658363
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-03-12 22:47:54 +0100

    ajout classe ChainsawTest + initialisation

---
+++ test/zombicide/item/weapon/ChainsawTest.java
@@ -0,0 +1,19 @@
+package zombicide.item.weapon;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChainsawTest {
+
+    @Test
+    public void testInitialization() {
+        Chainsaw chainsaw = new Chainsaw();
+        assertEquals(0, chainsaw.getRangeMin(), "Minimum range should be 0 for Pistol");
+        assertEquals(0, chainsaw.getRangeMax(), "Maximum range should be 1 for Pistol");
+        assertEquals(3, chainsaw.getHowManyDamage(), "Damage should be 1 for Pistol");
+        assertEquals(5, chainsaw.getThreshold(), "Threshold should be 4 for Pistol");
+    }
+
+
+}
