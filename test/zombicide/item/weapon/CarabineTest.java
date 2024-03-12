882d33ef4602b8ed0f461e7230f68ef84b280e0c
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-03-12 22:46:46 +0100

    ajout classe test Carabine + test initialisation

---
+++ test/zombicide/item/weapon/CarabineTest.java
@@ -0,0 +1,20 @@
+package zombicide.item.weapon;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarabineTest {
+
+    @Test
+    public void testInitialization() {
+        Carabine carabine = new Carabine();
+        assertEquals(1, carabine.getRangeMin(), "Minimum range should be 0 for Pistol");
+        assertEquals(3, carabine.getRangeMax(), "Maximum range should be 1 for Pistol");
+        assertEquals(1, carabine.getHowManyDamage(), "Damage should be 1 for Pistol");
+        assertEquals(4, carabine.getThreshold(), "Threshold should be 4 for Pistol");
+    }
+
+
+}
+
