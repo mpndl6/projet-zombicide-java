d77fe802b99d31247f94ada4ef0cd0a2949bbb43
youssef.chah.etu <youssef.chah.etu@univ-lille.fr>
2024-03-12 20:20:28 +0100

    Continental:constructeur

--- src/zombicide/map/cell/room/Continental.java
+++ src/zombicide/map/cell/room/Continental.java
@@ -4,8 +4,8 @@ import zombicide.map.cell.*;
 import zombicide.map.util.*;
 
 public class Continental extends SpecialRoom {
-    public Continental(Position p, int w,int h){
-        super(p,w,h);
+    public Continental(){
+        super();
     }
     /**
      * @return true if it's possible to fight in this area
