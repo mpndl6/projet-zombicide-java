1aa77d3daa8ac970f0986d83a7896c2385e5f0bd
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-03-12 22:44:57 +0100

    ajout test classe Axe, test initialisation

---
+++ test/zombicide/item/weapon/AxeTest.java
@@ -0,0 +1,18 @@
+package zombicide.item.weapon;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AxeTest {
+    @Test
+    public void testInitialization() {
+        Axe axe = new Axe();
+        assertEquals(0, axe.getRangeMin(), "Minimum range should be 0 for Pistol");
+        assertEquals(0, axe.getRangeMax(), "Maximum range should be 1 for Pistol");
+        assertEquals(2, axe.getHowManyDamage(), "Damage should be 1 for Pistol");
+        assertEquals(4, axe.getThreshold(), "Threshold should be 4 for Pistol");
+    }
+
+
+}
