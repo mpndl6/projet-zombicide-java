eb26e4a923152389ec9fbe16dadef40c95f071cc
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-13 19:00:25 +0100

    Second livrable en cours

--- src/zombicide/map/MainSecondDeliverable.java
+++ src/zombicide/map/MainSecondDeliverable.java
@@ -5,89 +5,104 @@ import zombicide.actor.survivors.type.Fighter;
 import zombicide.actor.survivors.type.Healer;
 import zombicide.actor.survivors.type.Lucky;
 import zombicide.actor.survivors.type.Nosy;
+import zombicide.actor.zombies.Zombie;
+import zombicide.actor.zombies.type.Abomination;
+import zombicide.actor.zombies.type.Powerful;
+import zombicide.actor.zombies.type.Runner;
+import zombicide.actor.zombies.type.Walker;
 import zombicide.item.utility.Plan;
 import zombicide.item.utility.Vial;
 import zombicide.map.cell.room.Continental;
 import zombicide.map.cell.room.DrugStore;
+import zombicide.map.util.Location;
 import zombicide.map.util.Position;
 import zombicide.map.cell.*;
 
 import java.util.*;
 
+/**
+ * the second deliverable of the project.
+ * Based on subclass Training map where there's method used for this deliverable but won't be used for the final
+ * conception.
+ * It's for the purpose of expansion and avoid to change the code when deliverable it's over.
+ */
+public class MainSecondDeliverable {
 
-public class Livrable2 {
     public static void main(String [] args){
 
         Scanner scanner = new Scanner(System.in);
-        //Ville d'entraienement :
-        int weightOfMap= 5;
-        int heightOfMap=5;
-        int weightOfCells=7;
+        //Training map :
+        int weightOfCells=10;
 
+        //Add of special room and special street
         List<Cell> listrooms = new ArrayList<Cell>();
         List<Cell> liststreets = new ArrayList<Cell>();
 
         liststreets.add(new StreetWW());
         listrooms.add(new Continental());
-        listrooms.add(new DrugStore());
+       // listrooms.add(new DrugStore());
 
 
-        Map map = new Map(weightOfMap,heightOfMap,listrooms,liststreets);
+        TrainingMap2 map = new TrainingMap2(listrooms,liststreets);
 
+        //new survivors constrcuted
         Survivor survivor1 = new Healer("Dr.Dre");
         Survivor survivor2 = new Nosy("snoop");
         Survivor survivor3= new Fighter("Meek");
         Survivor survivor4 = new Lucky("zz");
 
         Position crossRoadPos = map.getPrincipalIntersection();
-        Cell crossRoad = map.getCell(crossRoadPos);
-        //ajout des joueurs sur la cellules
+
+        //add of players on principal street
         map.putActorONCell(survivor1,crossRoadPos);
         map.putActorONCell(survivor2, crossRoadPos);
         map.putActorONCell(survivor3, crossRoadPos);
         map.putActorONCell(survivor4, crossRoadPos);
 
-
-        //ajout des fioles en main
+        //add of vials in hand
         survivor1.putInHand(new Vial());
         survivor2.putInHand(new Vial());
         survivor3.putInHand(new Vial());
         survivor4.putInHand(new Vial());
 
-        //ajout des carte dans le sac
+        //add of the plan on backpack
         survivor1.putItemInBackpack(new Plan(map));
         survivor2.putItemInBackpack(new Plan(map));
         survivor3.putItemInBackpack(new Plan(map));
         survivor4.putItemInBackpack(new Plan(map));
 
-        // premier affichage
+        System.out.println("first display of the map:");
         Grid grid = new Grid(map, weightOfCells);
         grid.displayGrid();
+        System.out.println(); //espace
         // pour l'instant les joueurs sont sur la map (a4 : 4 acteurs)
 
         //dans le deuxieme affichage déplacer les joueurs vers la case du nord mais il faut un obj pour ouvrir une porte je pense
 
         // En attendant la création des utilitaires (Ajout de la carte dans le backpack + de la fiole dans la main:
 
-      List<Survivor> listSurvivor = new ArrayList<Survivor>();
-        listSurvivor.add(survivor1);
-        listSurvivor.add(survivor2);
-        listSurvivor.add(survivor3);
-        listSurvivor.add(survivor4);
-
-        for(Survivor s : listSurvivor){
-            Cell currentCell = s.getCell();
-            Position positionCellAbove = new Position(currentCell.getPosition().getX()-1, currentCell.getPosition().getY());
-            Cell cellAbove = map.getCells()[currentCell.getPosition().getX()-1][currentCell.getPosition().getY()];
-            Position pCell = cellAbove.getPosition();
-           map.putActorONCell(s,pCell);
-
-        }
-        System.out.println();
+        //survivors deplacement upside
+       map.moveActor(survivor1,Location.NORTH);
+        map.moveActor(survivor2,Location.NORTH);
+        map.moveActor(survivor3,Location.NORTH);
+        map.moveActor(survivor4,Location.NORTH);
         grid.displayGrid();
 
+        //test survivor1 deplacement to the right
+        map.openDoorOfRoom(survivor1.getCell(), Location.EAST); // open the door
+        map.moveActor(survivor1, Location.EAST);
 
+        //construction zombie
+        Zombie abomination = new Abomination();
+        Zombie runner = new Runner();
+        Zombie powerful = new Powerful();
+        Zombie walker = new Walker();
 
+        map.putActorONCell(abomination, map.getPositionOFWaster(Location.NORTH));
+        map.putActorONCell(runner, map.getPositionOFWaster(Location.EAST));
+        map.putActorONCell(powerful, map.getPositionOFWaster(Location.WEST));
+        map.putActorONCell(walker, map.getPositionOFWaster(Location.SOUTH));
 
+        grid.displayGrid();
     }
 }
