32418fce243efbd9e9d25ac18bd0c417a7ab435d
youssef.chah.etu <youssef.chah.etu@univ-lille.fr>
2024-03-13 12:18:15 +0100

    description

--- src/zombicide/item/utility/IRGoogles.java
+++ src/zombicide/item/utility/IRGoogles.java
@@ -17,12 +17,12 @@ public class IRGoogles extends Utility {
     public void use(){
         Position p = this.survivor.getCell().getPosition();
         if(p.getX() + 1 < this.map.getWidth())
-            System.out.printf(this.map.getCells()[p.getX() + 1][p.getY()].description());
+            System.out.printf("WEST\n"+this.map.getCells()[p.getX() + 1][p.getY()].description());
         if(p.getX() - 1 >= 0)
-            System.out.printf(this.map.getCells()[p.getX() - 1][p.getY()].description());
+            System.out.printf("EAST\n"+this.map.getCells()[p.getX() - 1][p.getY()].description());
         if(p.getY() + 1 < this.map.getHeight())
-            System.out.printf(this.map.getCells()[p.getX()][p.getY() + 1].description());
+            System.out.printf("SOUTH\n"+this.map.getCells()[p.getX()][p.getY() + 1].description());
         if(p.getY() - 1 >= 0)
-            System.out.printf(this.map.getCells()[p.getX()][p.getY() - 1].description());
+            System.out.printf("NORTH\n"+this.map.getCells()[p.getX()][p.getY() - 1].description());
     }
 }
