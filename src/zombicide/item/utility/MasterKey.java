bd00e24798b30ea44f504c4072350ebfa238e7a7
youssef.chah.etu <youssef.chah.etu@univ-lille.fr>
2024-03-13 11:24:00 +0100

    update MasterKey Methode use

--- src/zombicide/item/utility/MasterKey.java
+++ src/zombicide/item/utility/MasterKey.java
@@ -1,12 +1,17 @@
 package zombicide.item.utility;
 
 import zombicide.map.cell.Room;
+import zombicide.map.cell.Street;
 import zombicide.map.cell.util.CellType;
 import zombicide.map.util.Location;
+import zombicide.map.*;
+import zombicide.map.util.Position;
 
 public class MasterKey extends Utility{
-
-    public MasterKey() {}
+    protected Map map;
+    public MasterKey(Map map) {
+        this.map = map;
+    }
 
     /**
      * Uses the master key utility item to open a door at the specified location.
@@ -14,9 +19,23 @@ public class MasterKey extends Utility{
      * @param l The location of the door to be opened.
      */
     public void use(Location l) {
-        if (this.survivor.getCell().getTypeOfCell() == CellType.ROOM){
-            Room room = (Room) this.survivor.getCell();
-            room.openDoor(l);
+        if (this.survivor.getCell() instanceof Room) {
+            this.map.openDoorOfRoom(this.survivor.getCell(), l);
+        }
+        if (this.survivor.getCell() instanceof Street){
+            Position poOfSurvivor = this.survivor.getCell().getPosition();
+            if(l == Location.NORTH){
+                this.map.openDoorOfRoom(map.getCell(new Position(poOfSurvivor.getX(),poOfSurvivor.getY()-1)),Location.SOUTH);
+            }
+            if(l == Location.SOUTH) {
+                this.map.openDoorOfRoom(map.getCell(new Position(poOfSurvivor.getX(),poOfSurvivor.getY()+1)),Location.NORTH);
+            }
+            if(l == Location.EAST) {
+                this.map.openDoorOfRoom(map.getCell(new Position(poOfSurvivor.getX()+1,poOfSurvivor.getY())),Location.WEST);
+            }
+            if(l == Location.WEST) {
+                this.map.openDoorOfRoom(map.getCell(new Position(poOfSurvivor.getX()-1,poOfSurvivor.getY())),Location.EAST);
+            }
         }
     }
 }
