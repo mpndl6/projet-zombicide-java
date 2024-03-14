f713e94af006593ea6a3bf6ed7dd67d3bcbae6a5
Annie Neilla <annieneilla.kobako.etu>
2024-03-14 05:27:38 +0100

    ajout de la classe runner et Abomination

--- src/zombicide/actor/zombies/type/Abomination.java
+++ src/zombicide/actor/zombies/type/Abomination.java
@@ -1,11 +1,15 @@
 package zombicide.actor.zombies.type;
 import zombicide.actor.zombies.Zombie;
+
+/**
+ * The abomination represents a type of zombie called abomination in the zombicide game
+ */
 public class Abomination extends Zombie {
     public static final int ABOMINATION_DAMAGES = 3;
     public static final String ABOMINATION_NN = "Abomination";
 
     /**
-     *
+     *Constructs a new abomination zombie with default parameters
      */
     public Abomination() {
         super(ABOMINATION_NN, ABOMINATION_DAMAGES, 6);
--- src/zombicide/actor/zombies/type/Runner.java
+++ src/zombicide/actor/zombies/type/Runner.java
@@ -4,7 +4,7 @@ import zombicide.actor.zombies.Zombie;
 
 public class Runner extends Zombie{
     public static final int RUNNER_DAMAGES=1;
-    public static final String RUNNER_NN = "Ruuner";
+    public static final String RUNNER_NN = "Runner";
     public Runner(){
         super(RUNNER_NN,RUNNER_DAMAGES,2);
     }
