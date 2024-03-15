e5fb31d392e58e4bf604bfb6a05c2c5606b3da89
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-15 20:18:42 +0100

    suppression attributs inutiles dans la classe test

--- test/zombicide/map/cell/room/DrugStoreTest.java
+++ test/zombicide/map/cell/room/DrugStoreTest.java
@@ -1,19 +1,12 @@
 package zombicide.map.cell.room;
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Before;
 import org.junit.Test;
-import zombicide.map.util.Position;
 
 public class DrugStoreTest {
-
     private DrugStore drugstore;
-
     @Before
     public void before() {
-        Position position = new Position(2, 2);
-        int width = 4;
-        int height = 4;
         drugstore = new DrugStore();
     }
 
