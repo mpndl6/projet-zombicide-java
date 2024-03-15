5ec70aea7441801ce9ae4ae06ec73e5036a56db5
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-15 16:40:37 +0100

    séparation test en plusieurs

--- test/zombicide/item/weapon/PistolTest.java
+++ test/zombicide/item/weapon/PistolTest.java
@@ -5,11 +5,26 @@ import org.junit.Test;
 public class PistolTest {
 
     @Test
-    public void testInitialization() {
+    public void testRangeMinInitialization() {
         Pistol pistol = new Pistol();
         assertEquals(0, pistol.getRangeMin());
+    }
+
+    @Test
+    public void testRangeMaxInitialization() {
+        Pistol pistol = new Pistol();
         assertEquals(1, pistol.getRangeMax());
+    }
+
+    @Test
+    public void testHowManyDamageInitialization() {
+        Pistol pistol = new Pistol();
         assertEquals(1, pistol.getHowManyDamage());
+    }
+
+    @Test
+    public void testThresholdInitialization() {
+        Pistol pistol = new Pistol();
         assertEquals(4, pistol.getThreshold());
     }
 
