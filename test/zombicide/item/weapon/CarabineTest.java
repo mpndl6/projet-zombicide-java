2e8b74455978ce3bd059ef5d77d1100af7454de3
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-15 16:38:42 +0100

    séparation test en plusieurs

--- test/zombicide/item/weapon/CarabineTest.java
+++ test/zombicide/item/weapon/CarabineTest.java
@@ -6,14 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarabineTest {
 
-    @Test
-    public void testInitialization() {
-        Carabine carabine = new Carabine();
-        assertEquals(1, carabine.getRangeMin());
-        assertEquals(3, carabine.getRangeMax());
-        assertEquals(1, carabine.getHowManyDamage());
-        assertEquals(4, carabine.getThreshold());
-    }
+        @Test
+        public void testRangeMinInitialization() {
+            Carabine carabine = new Carabine();
+            assertEquals(1, carabine.getRangeMin());
+        }
+
+        @Test
+        public void testRangeMaxInitialization() {
+            Carabine carabine = new Carabine();
+            assertEquals(3, carabine.getRangeMax());
+        }
+
+        @Test
+        public void testHowManyDamageInitialization() {
+            Carabine carabine = new Carabine();
+            assertEquals(1, carabine.getHowManyDamage());
+        }
+
+        @Test
+        public void testThresholdInitialization() {
+            Carabine carabine = new Carabine();
+            assertEquals(4, carabine.getThreshold());
+        }
 
     @Test
     public void testHowManyThrows() {
