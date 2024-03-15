7dcfd21dfcee474d7bd1fe4322e923a6dade02d4
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-15 21:12:32 +0100

    class MapTest terminée

--- test/zombicide/map/MapTest.java
+++ test/zombicide/map/MapTest.java
@@ -1,43 +1,100 @@
 package zombicide.map;
 
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import zombicide.actor.survivors.Survivor;
 import zombicide.map.cell.Cell;
+import zombicide.map.cell.Room;
 import zombicide.map.cell.StreetWW;
 import zombicide.map.cell.room.Continental;
+import zombicide.map.util.Position;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapTest {
 
-    private Map testMap;
+    private Map map;
     private List<Cell> listrooms = new ArrayList<Cell>();
     private List<Cell> liststreets = new ArrayList<Cell>();
+    private Survivor survivor = new Survivor("ju");
 
     @Before
     public void before() {
         liststreets.add(new StreetWW());
         listrooms.add(new Continental());
-        testMap = new Map(5,5,listrooms,liststreets);
+        map = new Map(5,5,listrooms,liststreets);
     }
 
     @Test
     public void testGetWidth() {
-        assertEquals(10, testMap.getWidth());
+        assertEquals(5, map.getWidth());
     }
 
     @Test
     public void testGetHeight() {
-        assertEquals(10, testMap.getHeight());
+        assertEquals(5, map.getHeight());
     }
 
     @Test
-    public void testGeneratNumberForInitmap() {
-        int rand = testMap.generateNumberForInitmap(9, 2);
-        assertTrue(rand >= 2 && rand <= 9);
+    public void testGetCells(){
+        listrooms = new ArrayList<>();
+        liststreets = new ArrayList<>();
+        map = new Map(5,5,listrooms,liststreets);
+        // with a map without any special room nor special street, and of width 5 and height 5 it should be like this:
+        /*
+            ――――――――――――――――――――――――――――――――――――――――――――――――――
+            █R        ❘R        ❘ W       ❘R        ❘R        █
+            █         ❘         ❘         ❘         ❘         █
+            ――――――――――――――――――――          ――――――――――――――――――――
+            █R        ❘R        ❘         ❘R        ❘R        █
+            █         ❘         ❘         ❘         ❘         █
+            ――――――――――――――――――――          ――――――――――――――――――――
+            █ W                                       W       █
+            █                                                 █
+            ――――――――――――――――――――          ――――――――――――――――――――
+            █R        ❘R        ❘         ❘R        ❘R        █
+            █         ❘         ❘         ❘         ❘         █
+            ――――――――――――――――――――          ――――――――――――――――――――
+            █R        ❘R        ❘ W       ❘R        ❘R        █
+            █         ❘         ❘         ❘         ❘         █
+            ――――――――――――――――――――――――――――――――――――――――――――――――――
+         */
+        Cell[][] cellOfMap = map.getCells();
+        assertTrue(cellOfMap[0][0] instanceof Room);
+        assertTrue(cellOfMap[2][4] instanceof StreetWW);
+        assertTrue(cellOfMap[4][4] instanceof Room);
     }
+    @Test
+    public void testGetCell(){
+        map.putActorONCell(survivor, new Position(1,3));
+        assertSame(survivor.getCell(), map.getCell(new Position(1,3)));
+    }
+
+    @Test
+    public void testPutActorOnCell(){
+        map.putActorONCell(survivor, new Position(2,3));
+        assertSame(survivor.getCell(),map.getCell(new Position(2,3))); // survivor has been put on cell ppos (2,3)
+        map.putActorONCell(survivor, new Position(4,3));
+        assertSame(survivor.getCell(),map.getCell(new Position(4,3))); // survivor has been put on cell ppos (4,3)
+    }
+
+    @Test
+    public void testWhenActorIsPutOnCellTheyAreNotInTheirPreviousCell(){
+        map.putActorONCell(survivor, new Position(2,3));
+        assertSame(survivor.getCell(),map.getCell(new Position(2,3))); // survivor has been put on cell ppos (2,3)
+        map.putActorONCell(survivor, new Position(4,3));
+        assertNotSame(survivor.getCell(),map.getCell(new Position(2,3))); // survivor has been put on cell ppos (4,3) and they are not on cell 2,3 anymore.
+    }
+
+    @Test
+    public void testGetPrincipalInterSection(){
+        //pour une map 5,5 les coordonées de la rue principale  devrait être 2,2
+        assertEquals(map.getPrincipalIntersection(), new Position(2,2));
+    }
+
+
 }
