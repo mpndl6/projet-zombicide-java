ad424ea22e3dda8e2db30fd6700ad8805f23d803
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-15 16:40:02 +0100

    séparation test en plusieurs

--- test/zombicide/item/weapon/CrowbarTest.java
+++ test/zombicide/item/weapon/CrowbarTest.java
@@ -5,11 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CrowbarTest {
     @Test
-    public void testInitialization() {
+    public void testRangeMinInitialization() {
         Crowbar crowbar = new Crowbar();
         assertEquals(0, crowbar.getRangeMin());
+    }
+
+    @Test
+    public void testRangeMaxInitialization() {
+        Crowbar crowbar = new Crowbar();
         assertEquals(0, crowbar.getRangeMax());
+    }
+
+    @Test
+    public void testHowManyDamageInitialization() {
+        Crowbar crowbar = new Crowbar();
         assertEquals(1, crowbar.getHowManyDamage());
+    }
+
+    @Test
+    public void testThresholdInitialization() {
+        Crowbar crowbar = new Crowbar();
         assertEquals(4, crowbar.getThreshold());
     }
 
