9621cedb914bfaad0ca2c6f92a55c42a332eb31a
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-15 16:39:30 +0100

    séparation test en plusieurs

--- test/zombicide/item/weapon/ChainsawTest.java
+++ test/zombicide/item/weapon/ChainsawTest.java
@@ -7,11 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ChainsawTest {
 
     @Test
-    public void testInitialization() {
+    public void testRangeMinInitialization() {
         Chainsaw chainsaw = new Chainsaw();
         assertEquals(0, chainsaw.getRangeMin());
+    }
+
+    @Test
+    public void testRangeMaxInitialization() {
+        Chainsaw chainsaw = new Chainsaw();
         assertEquals(0, chainsaw.getRangeMax());
+    }
+
+    @Test
+    public void testHowManyDamageInitialization() {
+        Chainsaw chainsaw = new Chainsaw();
         assertEquals(3, chainsaw.getHowManyDamage());
+    }
+
+    @Test
+    public void testThresholdInitialization() {
+        Chainsaw chainsaw = new Chainsaw();
         assertEquals(5, chainsaw.getThreshold());
     }
 
