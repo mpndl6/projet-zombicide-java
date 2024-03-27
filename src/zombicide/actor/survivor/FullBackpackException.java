7e9059b9df23eb50fb11d407216a7caea7d090bf
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-27 11:23:20 +0100

    Ajout de la classe exception pour gérer les sacs à dos trop plein

---
+++ src/zombicide/actor/survivor/FullBackpackException.java
@@ -0,0 +1,12 @@
+package zombicide.actor.survivor;
+
+/** Class FullBackpackException util when an item does not exist in a particular place */
+public class FullBackpackException extends Exception{
+
+    public FullBackpackException (String message){
+        super(message);
+    }
+    public FullBackpackException(){
+        super();
+    }
+}
