d95cef0985149025941e5babbe2c1ce8736a45ec
youssef.chah.etu <youssef.chah.etu@univ-lille.fr>
2024-03-27 12:03:39 +0100

    interface Action

--- src/zombicide/action/Action.java
+++ src/zombicide/action/Action.java
@@ -1,24 +1,5 @@
 package zombicide.action;
 
-import zombicide.actor.survivor.Survivor;
-
-public abstract class Action {
-
-    protected int cost;
-    protected Survivor survivor;
-
-    public Action(Survivor s) {
-        this.cost = 1;
-        this.survivor = s;
-    }
-
-    public int getCost(){
-        return this.cost;
-    }
-
-    public Survivor getSurvivor(){
-        return this.survivor;
-    }
-
-    public abstract void make();
+public interface Action {
+    public void make();
 }
