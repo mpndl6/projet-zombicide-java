589995ec30eb35a2eb9ec84c8105026502f16319
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-28 12:54:35 +0100

    Mise à jour des tests des zombies

--- test/zombicide/actor/zombie/type/WalkerTest.java
+++ test/zombicide/actor/zombie/type/WalkerTest.java
@@ -1,15 +1,46 @@
 package zombicide.actor.zombie.type;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import zombicide.actor.ActorType;
+import zombicide.actor.survivor.Survivor;
+import zombicide.actor.zombie.Zombie;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WalkerTest {
+    private Zombie walker;
+
+    @BeforeEach
+    public void setUp(){
+        walker = new Walker();
+    }
 
     @Test
-    public void testConstructor() {
-        Walker walker = new Walker();
+    public void walkerHasOneLifePOint(){
+        assertEquals(1, walker.getLifePoints() );
+    }
+
+    @Test
+    public void walkerInflictsOneDamage(){
         assertEquals(1, walker.howManyDamages());
+    }
+
+    @Test
+    public void walkerIsTypeOfZombie(){
         assertEquals(ActorType.ZOMBIE, walker.getTypeOfActor());
+    }
+
+    @Test
+    public void testLifePointDecrease(){
+        Zombie zombie = new Walker();
+        zombie.decreaseLifePoints(); // a survivor has initially 5 life points at creation
+        assertEquals(0, zombie.getLifePoints());
+    }
+
+    @Test
+    public void walkerIsAliveAtCreation(){
+        assertTrue(walker.isAlive());
+
     }
 }
