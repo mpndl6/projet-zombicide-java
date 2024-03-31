b1c9a82f51bed1c79084b51fc0195a1103440c3b
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-31 16:23:43 +0200

    Documentation de la classe Action

--- src/zombicide/action/Action.java
+++ src/zombicide/action/Action.java
@@ -1,7 +1,16 @@
 package zombicide.action;
 
+/**
+ * Interface Action represents all the actions doable by the actors
+ */
 public interface Action {
+    /**
+     * Performs the action associated with this object.
+     * This method should be implemented by concrete classes to define
+     * specific behavior for the action.
+     */
     public void make();
 }
+
 
 
