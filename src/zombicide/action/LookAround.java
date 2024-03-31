6177735da20d3362b5c456b2bc60b4554a2ad95b
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-31 16:32:26 +0200

    Documentation de la classe LookAround

--- src/zombicide/action/LookAround.java
+++ src/zombicide/action/LookAround.java
@@ -2,14 +2,29 @@ package zombicide.action;
 
 import zombicide.actor.survivor.Survivor;
 
+/**
+ * The LookAround class represents an action where a Survivor looks around.
+ * This action allows the Survivor to get a description of the cell they are currently in.
+ * This class extends the ActionSurvivor abstract class.
+ */
 public class LookAround extends ActionSurvivor {
-    public LookAround(Survivor s) {
-        super(s);
-        this.cost = 0;
-    }
 
-    @Override
-    public void make() {
-        this.survivor.getCell().description();
-    }
+/**
+ * Constructs a new LookAround object with the specified Survivor.
+ * @param s the Survivor associated with this action.
+ */
+public LookAround(Survivor s) {
+    super(s);
+    this.cost = 0;
+}
+
+/**
+ * Performs the action of looking around.
+ * This method calls the description method of the cell the Survivor is currently in.
+ * It overrides the methode make of the superclass
+ */
+@Override
+public void make() {
+    this.survivor.getCell().description();
+}
 }
