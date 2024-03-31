0d1354aaf12bddcaf3f93bdaf149fa9a79265a30
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-31 16:25:57 +0200

    Documentation de la classe ActionSurvivor

--- src/zombicide/action/ActionSurvivor.java
+++ src/zombicide/action/ActionSurvivor.java
@@ -2,24 +2,44 @@ package zombicide.action;
 
 import zombicide.actor.survivor.Survivor;
 
-public abstract class ActionSurvivor implements Action{
+/**
+ * The abstract class ActionSurvivor represents an action that can be performed by a Survivor.
+ * This class implements the Action interface.
+ */
+public abstract class ActionSurvivor implements Action {
+protected int cost; //The cost of performing this action.
 
-    protected int cost;
-    protected Survivor survivor;
+protected Survivor survivor; //The Survivor associated with this action.
 
-    public ActionSurvivor(Survivor s) {
-        this.cost = 1;
-        this.survivor = s;
-    }
-
-    public int getCost(){
-        return this.cost;
-    }
-
-    public Survivor getSurvivor(){
-        return this.survivor;
-    }
-
-    public abstract void make();
+/**
+ * Constructs a new ActionSurvivor object with the specified Survivor.
+ * @param s the Survivor associated with this action.
+ */
+public ActionSurvivor(Survivor s) {
+    this.cost = 1;
+    this.survivor = s;
 }
+
+/**
+ * Gets the cost of performing this action.
+ * @return the cost of performing this action.
+ */
+public int getCost() {
+    return this.cost;
+}
+
+/**
+ * Gets the Survivor associated with this action.
+ * @return the Survivor associated with this action.
+ */
+public Survivor getSurvivor() {
+    return this.survivor;
+}
+
+/**
+ * Abstract method to be implemented by subclasses to perform the action.
+ */
+public abstract void make();
+}
+
 
