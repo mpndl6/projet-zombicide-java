5b0a6a8d56de08d681ae148235d3e62f4a5dad5c
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-03-31 21:26:20 +0200

    Modification MakeNoise(Ajout condition) + doc

--- src/zombicide/action/MakeNoise.java
+++ src/zombicide/action/MakeNoise.java
@@ -3,14 +3,24 @@ package zombicide.action;
 import zombicide.actor.survivor.Survivor;
 
 public class MakeNoise extends ActionSurvivor{
-
+    /**
+     * Constructor for the makeNoise action.
+     *
+     * @param s The survivor performing the action
+     */
     public MakeNoise(Survivor s) {
         super(s);
 
     }
+
+    /**
+     * Performs the action of making noise by an survivor in the current room.
+     */
     @Override
     public void make() {
-        this.survivor.getCell().makenoise();
-
+        if(this.survivor.getActionPoint()>0) {
+            this.survivor.getCell().makenoise();
+            this.survivor.decreaseActionPoints();
+        }
     }
 }
