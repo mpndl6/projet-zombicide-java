5023658b1e53e8273f13653c26aa5f0bc8c5f551
Annie Neilla <annieneilla.kobako.etu>
2024-03-31 03:44:32 +0200

    ajout de la doc dans la classe search

--- src/zombicide/action/Search.java
+++ src/zombicide/action/Search.java
@@ -6,13 +6,23 @@ import zombicide.map.cell.Room;
 
 import java.util.List;
 
+/**
+ * Represents the action of searching for items in a room.
+ */
 public class Search extends ActionSurvivor {
-
+    /**
+     * Constructor for the search action.
+     *
+     * @param s The survivor performing the search.
+     */
     public Search(Survivor s) {
         super(s);
         this.cost = 1;
     }
 
+    /**
+     * Performs the action of searching for items in the current room.
+     */
     @Override
     public void make() {
         Room currentRoom = (Room) survivor.getCell();
