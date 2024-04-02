2124aa6fcaa4fbb713912849346eb6b7ae8e5ff4
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-02 22:51:45 +0200

    ajout de la méthode canMakeAction et changement de la méthode pour prendre en compte cette méthode Point du survivant diminués après utilisation de l'action

--- src/zombicide/action/MakeNoise.java
+++ src/zombicide/action/MakeNoise.java
@@ -1,6 +1,8 @@
 package zombicide.action;
 
+import exception.NoSuchItemException;
 import zombicide.actor.survivor.Survivor;
+import zombicide.callable.Callable;
 
 public class MakeNoise extends ActionSurvivor{
     /**
@@ -10,17 +12,24 @@ public class MakeNoise extends ActionSurvivor{
      */
     public MakeNoise(Survivor s) {
         super(s);
+        this.survivor.decreaseActionPoints(super.cost);
+    }
 
+    @Override
+    public boolean canMakeAction() {
+        return true;
     }
 
     /**
      * Performs the action of making noise by an survivor in the current room.
      */
     @Override
-    public void make() {
-        if(this.survivor.getActionPoint()>0) {
+    public boolean make(Callable callable) throws Exception {
+        if(this.canMakeAction()) {
             this.survivor.getCell().makenoise();
-            this.survivor.decreaseActionPoints();
+            this.survivor.decreaseActionPoints(this.getCost());
+            return true;
         }
+        return false;
     }
 }
