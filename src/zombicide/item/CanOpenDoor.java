8656a7df46880c47f64d63134b9739cb8cef49f6
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-02 22:49:01 +0200

    Ajout de l'interface CanOpenDoor pour les objets qui peuvent ouvrir les portes

---
+++ src/zombicide/item/CanOpenDoor.java
@@ -0,0 +1,12 @@
+package zombicide.item;
+
+import exception.IsWallException;
+import zombicide.map.util.Location;
+
+public interface CanOpenDoor {
+
+    /**
+     *
+     */
+    public void open(Location location);
+}
