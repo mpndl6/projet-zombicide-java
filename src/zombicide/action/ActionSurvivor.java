f00ba4f2578c8cc88632855375f3db40ceba05bc
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-02 22:48:09 +0200

    Ajout de la méthode canMakeAction et changement de la signature de make

--- src/zombicide/action/ActionSurvivor.java
+++ src/zombicide/action/ActionSurvivor.java
@@ -1,45 +1,52 @@
 package zombicide.action;
 
+import exception.NoSuchItemException;
 import zombicide.actor.survivor.Survivor;
+import zombicide.callable.Callable;
 
 /**
  * The abstract class ActionSurvivor represents an action that can be performed by a Survivor.
  * This class implements the Action interface.
  */
 public abstract class ActionSurvivor implements Action {
-protected int cost; //The cost of performing this action.
+    protected int cost; //The cost of performing this action.
 
-protected Survivor survivor; //The Survivor associated with this action.
+    protected Survivor survivor; //The Survivor associated with this action.
 
-/**
- * Constructs a new ActionSurvivor object with the specified Survivor.
- * @param s the Survivor associated with this action.
- */
-public ActionSurvivor(Survivor s) {
-    this.cost = 1;
-    this.survivor = s;
-}
+    /**
+     * Constructs a new ActionSurvivor object with the specified Survivor.
+     * @param s the Survivor associated with this action.
+     */
+    public ActionSurvivor(Survivor s) {
+        this.cost = 1;
+        this.survivor = s;
+    }
 
-/**
- * Gets the cost of performing this action.
- * @return the cost of performing this action.
- */
-public int getCost() {
-    return this.cost;
-}
+    /**
+     *
+     */
+    public abstract boolean canMakeAction();
 
-/**
- * Gets the Survivor associated with this action.
- * @return the Survivor associated with this action.
- */
-public Survivor getSurvivor() {
-    return this.survivor;
-}
+    /**
+     * Gets the cost of performing this action.
+     * @return the cost of performing this action.
+     */
+    public int getCost() {
+        return this.cost;
+    }
 
-/**
- * Abstract method to be implemented by subclasses to perform the action.
- */
-public abstract void make();
+    /**
+     * Gets the Survivor associated with this action.
+     * @return the Survivor associated with this action.
+     */
+    public Survivor getSurvivor() {
+        return this.survivor;
+    }
+
+    /**
+     * Abstract method to be implemented by subclasses to perform the action.
+     */
+    public abstract boolean make(Callable callable) throws Exception;
 }
 
 
