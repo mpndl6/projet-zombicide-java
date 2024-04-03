777d94a3e11d9061da7d49bdc1ddd7eb65ed89e2
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-04 01:32:15 +0200

    clase Armory non terminée

--- src/zombicide/map/cell/room/Armory.java
+++ src/zombicide/map/cell/room/Armory.java
@@ -1,9 +1,43 @@
 package zombicide.map.cell.room;
 
+import zombicide.actor.Actor;
+import zombicide.actor.PNJ;
+import zombicide.actor.zombie.Zombie;
+
 public class Armory extends SpecialRoom{
+
+    protected PNJ pnj;
 
     public Armory(){
         super();
+        this.pnj = null;
     }
+
+    /**
+     *
+     */
+    /**
+     * Adds an actor to the cell
+     *@param a to be added to the cell
+     */
+    public void welcomeActor(Actor a){
+        if(a instanceof Zombie)
+            return;
+
+        boolean hasSucced = this.pnj.welcomeSurvivor();
+        if(hasSucced){
+            this.pnj.fabricate();
+        }
+
+    }
+
+    /**
+     *
+     */
+    public void setPNJ(PNJ p){
+        this.pnj = p;
+    }
+
+
 
 }
