a3c243eea2b1349ad329a8d6162490a5c2e34984
youssef.chah.etu <youssef.chah.etu@univ-lille.fr>
2024-04-03 11:59:04 +0200

    class game + methode doorsBetween

---
+++ src/zombicide/Game.java
@@ -0,0 +1,57 @@
+package zombicide;
+
+import zombicide.actor.survivor.Survivor;
+import zombicide.actor.zombie.Zombie;
+import zombicide.map.Map;
+import zombicide.map.util.Location;
+import zombicide.map.util.Position;
+
+import java.util.*;
+
+
+public class Game {
+    protected Map map;
+    protected List<Survivor> listSurvivors;
+    protected List<Zombie> listZombies;
+
+    public Game(Map map) {
+        this.map = map;
+        this.listSurvivors = new ArrayList<>();
+        this.listZombies = new ArrayList<>();
+    }
+
+    public boolean doorsBetween(Position p1, Position p2) {
+        int x1 = p1.getX();
+        int y1 = p1.getY();
+        int x2 = p2.getX();
+        int y2 = p2.getY();
+        boolean res = true;
+        if (x1 == x2 || y1 == y2){
+            if (x1 > x2) {
+                for (int i = x1; i > x2 ; i--) {
+                    res = res && this.map.isOpenDoor(this.map.getCell(new Position(i,y1)), Location.WEST);
+
+                }
+            }
+            if (x1 < x2) {
+                for (int i = x1; i < x2 ; i++) {
+                    res = res && this.map.isOpenDoor(this.map.getCell(new Position(i,y1)), Location.EAST);
+
+                }
+            }
+            if (y1 > y2) {
+                for (int i = y1; i > y2 ; i--) {
+                    res = res && this.map.isOpenDoor(this.map.getCell(new Position(x1,i)), Location.NORTH);
+
+                }
+            }
+            if (y1 < y2) {
+                for (int i = y1; i < y2 ; i++) {
+                    res = res && this.map.isOpenDoor(this.map.getCell(new Position(x1,i)), Location.SOUTH);
+
+                }
+            }
+        }
+        return res;
+    }
+}
