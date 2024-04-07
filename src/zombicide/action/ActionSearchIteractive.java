da92c2acdc3eaeee73cbf3a4d76d2b2bd96484ad
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-07 21:44:03 +0200

    classe ActionSearchIteractive TODO

--- src/zombicide/action/ActionSearchIteractive.java
+++ src/zombicide/action/ActionSearchIteractive.java
@@ -4,16 +4,18 @@ import zombicide.actor.survivor.Survivor;
 import zombicide.callable.Callable;
 import zombicide.map.cell.Cell;
 
-public class ActionSearchIteractive extends ActionSearch{
+public class ActionSearchIteractive extends ActionSearch {
     public ActionSearchIteractive(Survivor s) {
         super(s);
     }
 
     @Override
-    public boolean make(Callable callable) throws Exception{
+    public boolean make(Callable callable) throws Exception {
         if (!canMakeAction())
             return false;
         Cell survivorCell = survivor.getCell();
-
+        if (survivor.getAllInBackpack().size() < Survivor.MAX_NB_ITEM) {
+        } //TODO
+        return true;
     }
 }
