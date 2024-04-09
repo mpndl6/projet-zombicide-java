1f30254b522436ea4913d997c4e9b0c1934b0cd5
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-10 00:40:39 +0200

    Ajout de la documentation

--- src/zombicide/action/UseItem.java
+++ src/zombicide/action/UseItem.java
@@ -5,10 +5,12 @@ import zombicide.actor.survivor.Survivor;
 import zombicide.callable.Callable;
 import zombicide.item.Item;
 
+/**
+ * Permits to a survivor to use an Item
+ */
 public class UseItem extends ActionSurvivor {
     /**
      * Constructs a new ActionSurvivor object with the specified Survivor.
-     *
      * @param s the Survivor associated with this action.
      */
     public UseItem(Survivor s) {
@@ -25,19 +27,25 @@ public class UseItem extends ActionSurvivor {
         return super.survivor.getWhatINHand()!=null;
     }
 
-    /*
-    * Executes the action of using the item currently held by the survivor associated with this action
-
-    */
+    /**
+     * Method that permit to do the action of use any item
+     * @param callable a callable, here it's an instance of Item
+     * @return true if the action has been realise succesfully
+     */
     @Override
-    public boolean make(Callable callable) throws Exception {
+    public boolean make(Callable callable){
         if(!this.canMakeAction()) {
             System.out.println("There's no item in their hand.");
             return false;
         }
         Item i = super.survivor.getWhatINHand();
         //i.addSurvivor(this.survivor); <- déjà géré autrepart ça normalement
-        i.use(callable);
+        try {
+            i.use(callable);
+        }
+        catch (Exception e){
+            e.getMessage();
+        }
         return true;
         }
     }
