6f5586d2020e0add4989f33c31e74cee241ccaf9
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-04-09 22:19:18 +0200

    Ajout des autres test de positions pour moveaside

--- test/zombicide/action/MoveAsideTest.java
+++ test/zombicide/action/MoveAsideTest.java
@@ -40,4 +40,35 @@ public class MoveAsideTest {
         assertTrue(result);
         assertEquals(new Position(2, 1), cell.getPosition());
     }
+    @Test
+    public void make_MoveSurvivorEast_Successful() throws Exception {
+        Survivor survivor = new Survivor("Survivor1");
+        Cell cell = new Room();
+        cell.setPosition(new Position(1,1));
+
+        survivor.setCell(cell);
+        MoveAside moveAside = new MoveAside(survivor);
+
+        boolean result = moveAside.make(Location.EAST);
+
+        assertTrue(result);
+        assertEquals(new Position(1, 2), cell.getPosition());
+    }
+
+    @Test
+    public void make_MoveSurvivorWest_Successful() throws Exception {
+
+        Survivor survivor = new Survivor("Survivor1");
+        Cell cell = new Room();
+        cell.setPosition(new Position(1,1));
+        survivor.setCell(cell);
+        MoveAside moveAside = new MoveAside(survivor);
+
+
+        boolean result = moveAside.make(Location.WEST);
+
+
+        assertTrue(result);
+        assertEquals(new Position(1, 0), cell.getPosition());
+    }
 }
