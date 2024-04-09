abd77e8469d49c5219566ebefa26194b0ef8e1f4
Annie Neilla <annieneilla.kobako.etu>
2024-04-09 23:37:08 +0200

    ajout d'un nouveau item

--- src/zombicide/item/utility/CoverUp.java
+++ src/zombicide/item/utility/CoverUp.java
@@ -1,6 +1,16 @@
 package zombicide.item.utility;
 
+import zombicide.callable.Callable;
+import zombicide.map.Map;
+import zombicide.map.cell.Cell;
+import zombicide.map.util.Position;
+
 public class CoverUp extends Utility{
+    protected Map map;
+    public CoverUp(Map map){
+        this.map = map;
+    }
+
 
 
 
