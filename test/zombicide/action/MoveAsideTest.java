96e5038c71b93e745ccbe66d5601b90c30de0e59
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-04-10 21:56:48 +0200

    Correction test

--- test/zombicide/action/MoveAsideTest.java
+++ test/zombicide/action/MoveAsideTest.java
@@ -1,21 +1,32 @@
 package zombicide.action;
 
+import zombicide.Game;
 import zombicide.actor.survivor.Survivor;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
+import zombicide.map.Map;
+import zombicide.map.TrainingMap2;
 import zombicide.map.cell.Cell;
 import zombicide.map.cell.Room;
 import zombicide.map.util.Location;
 import zombicide.map.util.Position;
-/**
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoveAsideTest {
     @Test
     public void make_MoveSurvivorNorth_Successful()  {
 
         Survivor survivor = new Survivor("Survivor1");
-        Cell cell = new Room();
-        cell.setPosition(new Position(1,1));
-        survivor.setCell(cell);
+        List<Cell> listRooms = new ArrayList<Cell>();
+        List<Cell> listSreet = new ArrayList<Cell>();
+        Map map = new TrainingMap2(listRooms,listSreet);
+        Game game = new Game(map);
+        survivor.setGame(game);
+        map.putActorONCell(survivor,new Position(1,1));
+
+
         MoveAside moveAside = new MoveAside(survivor);
 
 
@@ -23,45 +34,52 @@ public class MoveAsideTest {
 
 
         assertTrue(result);
-        assertEquals(new Position(0, 1), cell.getPosition());
+        assertEquals(new Position(0, 1), survivor.getCell().getPosition());
     }
 
     @Test
     public void make_MoveSurvivorSouth_Successful()  {
         Survivor survivor = new Survivor("Survivor1");
-        Cell cell = new Room();
-        cell.setPosition(new Position(1,1));
-
-        survivor.setCell(cell);
+        List<Cell> listRooms = new ArrayList<Cell>();
+        List<Cell> listSreet = new ArrayList<Cell>();
+        Map map = new TrainingMap2(listRooms,listSreet);
+        Game game = new Game(map);
+        survivor.setGame(game);
+        map.putActorONCell(survivor,new Position(1,1));
         MoveAside moveAside = new MoveAside(survivor);
 
         boolean result = moveAside.make(Location.SOUTH);
 
         assertTrue(result);
-        assertEquals(new Position(2, 1), cell.getPosition());
+        assertEquals(new Position(2, 1), survivor.getCell().getPosition());
     }
     @Test
     public void make_MoveSurvivorEast_Successful()  {
         Survivor survivor = new Survivor("Survivor1");
-        Cell cell = new Room();
-        cell.setPosition(new Position(1,1));
-
-        survivor.setCell(cell);
+        List<Cell> listRooms = new ArrayList<Cell>();
+        List<Cell> listSreet = new ArrayList<Cell>();
+        Map map = new TrainingMap2(listRooms,listSreet);
+        Game game = new Game(map);
+        survivor.setGame(game);
+        map.putActorONCell(survivor,new Position(1,1));
         MoveAside moveAside = new MoveAside(survivor);
 
         boolean result = moveAside.make(Location.EAST);
 
         assertTrue(result);
-        assertEquals(new Position(1, 2), cell.getPosition());
+        assertEquals(new Position(1, 2), survivor.getCell().getPosition());
     }
 
     @Test
     public void make_MoveSurvivorWest_Successful()  {
 
         Survivor survivor = new Survivor("Survivor1");
-        Cell cell = new Room();
-        cell.setPosition(new Position(1,1));
-        survivor.setCell(cell);
+        List<Cell> listRooms = new ArrayList<Cell>();
+        List<Cell> listSreet = new ArrayList<Cell>();
+        Map map = new TrainingMap2(listRooms,listSreet);
+        Game game = new Game(map);
+        survivor.setGame(game);
+        map.putActorONCell(survivor,new Position(1,1));
         MoveAside moveAside = new MoveAside(survivor);
 
 
@@ -69,7 +87,6 @@ public class MoveAsideTest {
 
 
         assertTrue(result);
-        assertEquals(new Position(1, 0), cell.getPosition());
+        assertEquals(new Position(1, 0), survivor.getCell().getPosition());
     }
 }
-*/
