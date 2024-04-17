d1d991ef71a960dccff921650d557325fee663c3
Gabrielle Maponda Lendo <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-17 11:26:51 +0200

    ajout de la méthode utile randomLocation()

--- src/zombicide/map/util/Location.java
+++ src/zombicide/map/util/Location.java
@@ -1,10 +1,20 @@
 package zombicide.map.util;
-
 import zombicide.callable.Callable;
+
+import java.util.Random;
 
 /**
 * The Location enum represents cardinal directions.
 */
 public enum Location implements Callable {
 	   EAST,NORTH,WEST,SOUTH;
+	private static Random ALEA = new Random();
+
+	/**
+	 * Permits to retrieve a random location
+	 * @return a random Location [NORTH, SOUTH, EAST, WEST]
+	 */
+	public static Location randomLocation(){
+		return Location.values() [ ALEA.nextInt(Location.values().length) ];
+	}
 }
