7ba7b424d2c515c053cb63737ddb6fb12817ba2b
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-22 13:08:31 +0200

    suppression des 'public' inutiles

--- src/zombicide/action/Action.java
+++ src/zombicide/action/Action.java
@@ -7,25 +7,25 @@ import zombicide.callable.Callable;
 /**
  * Interface Action represents all the actions doable by the actors
  */
-public interface Action {
+public interface Action extends Callable {
     /**
      * Performs the action associated with this object.
      * This method should be implemented by concrete classes to define
      * specific behavior for the action.
      */
-    public boolean make(Callable callable);
+    boolean make(Callable callable);
 
     /**
      * Gets the cost of the action that will be required
      * This method should be implemented by concrete classes
      */
-    public int getCost();
+     int getCost();
 
     /**
     * Tells if the actionc can be realised
     * @return true if the action can be realised
      */
-    public boolean canMakeAction();
+     boolean canMakeAction();
 }
 
 
