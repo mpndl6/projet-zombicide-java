e2a15eb1be34cfd4d8475019a51d26a89e800571
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-22 13:40:38 +0200

    Passage des classe ActionZombie et ActionSurvivor en interface

--- src/zombicide/action/actionSurvivor/ActionSurvivor.java
+++ src/zombicide/action/actionSurvivor/ActionSurvivor.java
@@ -7,47 +7,21 @@ import zombicide.callable.Callable;
  * The abstract class ActionSurvivor represents an action that can be performed by a Survivor.
  * This class implements the Action interface.
  */
-public abstract class ActionSurvivor implements Action, Callable {
-    protected int cost; //The cost of performing this action.
-
-    protected Survivor survivor; //The Survivor associated with this action.
-
-    /**
-     * Constructs a new ActionSurvivor object with the specified Survivor.
-     * @param s the Survivor associated with this action.
-     */
-    public ActionSurvivor(Survivor s) {
-        this.cost = 1;
-        this.survivor = s;
-    }
+public interface ActionSurvivor extends Action {
 
     /**
      * This method will be useful in each action.
      * It will prevent if the survivor can do or not the action of the class wanted
      * @return true if the survivor had been able to do the action wanted
      */
-    public abstract boolean canMakeAction();
+    boolean canMakeAction();
 
     /**
      * Gets the cost of performing this action.
      * @return the cost of performing this action.
      */
-    public int getCost() {
-        return this.cost;
-    }
+     int getCost();
 
-    /**
-     * Gets the Survivor associated with this action.
-     * @return the Survivor associated with this action.
-     */
-    public Survivor getSurvivor() {
-        return this.survivor;
-    }
-
-    /**
-     * Abstract method to be implemented by subclasses to perform the action.
-     */
-    public abstract boolean make(Callable callable);
 }
 
 
--- src/zombicide/action/actionZombie/ActionZombie.java
+++ src/zombicide/action/actionZombie/ActionZombie.java
@@ -8,39 +8,8 @@ import zombicide.callable.Callable;
  * The abstract class ActionZombie represents an action that can be performed by a Zombie.
  * This class implements the Action interface.
  */
-public abstract class ActionZombie implements Action,Callable {
-    protected int cost; //The cost of performing this action
-    protected Zombie zombie; //The Zombie associated with this action.
+public interface ActionZombie extends Action {
 
-    /**
-     * Constructs a new ActionZombie object with the specified Zombie.
-     * @param z the Zombie associated with this action.
-     */
-    public ActionZombie(Zombie z) {
-        this.cost = 1;
-        this.zombie = z;
-    }
-
-    /**
-     * Gets the cost of performing this action.
-     * @return the cost of performing this action.
-     */
-    public int getCost() {
-        return this.cost;
-    }
-
-    /**
-     * Gets the Zombie associated with this action.
-     * @return the Zombie associated with this action.
-     */
-    public Zombie getZombie() {
-        return this.zombie;
-    }
-
-    /**
-     * Abstract method to be implemented by subclasses to perform the action.
-     */
-    public abstract boolean make(Callable callable);
 }
 
 
