790690392063d3084e8f425c06fc80a709746603
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-22 13:43:56 +0200

    modification et correction de la méthde make et ajout d'un attribut actor

--- src/zombicide/action/actionSurvivor/MoveAside.java
+++ src/zombicide/action/actionSurvivor/MoveAside.java
@@ -1,6 +1,9 @@
 package zombicide.action.actionSurvivor;
 
+import zombicide.action.Action;
 import zombicide.action.actionSurvivor.ActionSurvivor;
+import zombicide.action.actionZombie.ActionZombie;
+import zombicide.actor.Actor;
 import zombicide.actor.survivor.Survivor;
 import zombicide.callable.Callable;
 import zombicide.map.Map;
@@ -8,16 +11,18 @@ import zombicide.map.cell.*;
 import zombicide.map.util.*;
 
 /**
- * This method permits to the survivor to move aside.
+ * This method permits to an object Actor to move aside.
  */
-public class MoveAside extends ActionSurvivor {
+public class MoveAside implements ActionSurvivor, ActionZombie {
+
+    protected Actor actor;
 
     /**
      * Constructs a new ActionSurvivor object with the specified Survivor.
-     * @param s the Survivor associated with this action.
+     * @param a the Survivor associated with this action.
      */
-    public MoveAside(Survivor s) {
-        super(s);
+    public MoveAside(Actor a) {
+        this.actor = a;
     }
 
     /**
@@ -31,49 +36,59 @@ public class MoveAside extends ActionSurvivor {
     }
 
     /**
-     * This methode moves the survivor to a specific cell.
-     * Survivors have cell and those cell can have different positions.
+     * This methode moves the actor to a specific cell.
      * @param callable a callable, here it's an instance of Location
-     * @return true if the survivor has moved eventually
+     * @return true if the actor has moved eventually
      */
     @Override
     public boolean make(Callable callable){
-        Cell cell = survivor.getCell();
+        Cell cell = actor.getCell();
         Position p = cell.getPosition();
         int xCell = p.getX();
         int yCell = p.getY();
-        Map map = this.survivor.getGame().getMap();
+        Map map = this.actor.getGame().getMap();
 
-        if (!canMakeAction()) {
-            System.out.println("Survivor is trying to go outside of the Map!");
+        Location l = (Location)callable;
+        if (!canMakeAction()  ||map.isWall(cell,l) || !map.isOpenDoor(cell,l)) {
             return false;
         }
-        Location l = (Location)callable;
-        switch (l){
+
+        switch (l) {
             case NORTH:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.survivor,new Position(xCell-1, yCell));
-                    return true;
-                }
+                map.putActorONCell(this.actor, new Position(xCell - 1, yCell));
+                System.out.println("up");
+                return true;
             case SOUTH:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.survivor,new Position(xCell+1, yCell ));
-                    return true;
-                }
+                map.putActorONCell(this.actor, new Position(xCell + 1, yCell));
+                System.out.println("down");
+                return true;
             case EAST:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.survivor,new Position(xCell , yCell+1));
-                    return true;
-                }
+                map.putActorONCell(this.actor, new Position(xCell, yCell + 1));
+                System.out.println("east");
+                return true;
             case WEST:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.survivor,new Position(xCell , yCell -1));
-                    return true;
-                }
+                map.putActorONCell(this.actor, new Position(xCell, yCell - 1));
+                System.out.println("west");
+                return true;
+
             default:
-                System.out.println("le survivant ne peut pas bouger");
+                System.out.println("le zombie ne peut pas bouger");
                 return false;
 
         }
+    }
+
+    /**
+     * Retrieves the cost of the action MoveAside
+     * @return the cost of this action
+     */
+    @Override
+    public int getCost() {
+        return 0;
+    }
+
+    @Override
+    public Actor getActor() {
+        return this.actor;
     }
 }
