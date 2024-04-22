1646b1f6b1e742a715c2854f33fc63440d8c291e
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-22 13:43:12 +0200

    héritage de la classe moveAside dans MoveAsideZombie

--- src/zombicide/action/actionZombie/MoveAsideZombie.java
+++ src/zombicide/action/actionZombie/MoveAsideZombie.java
@@ -1,8 +1,8 @@
 package zombicide.action.actionZombie;
 
+import zombicide.action.actionSurvivor.MoveAside;
 import zombicide.actor.zombie.Zombie;
 import zombicide.callable.Callable;
-import zombicide.map.Map;
 import zombicide.map.cell.Cell;
 import zombicide.map.util.Location;
 import zombicide.map.util.Position;
@@ -12,7 +12,7 @@ import java.util.Random;
 /**
  * Class MoveAside for the zombies
  */
-public class MoveAsideZombie extends AttackZombie {
+public class MoveAsideZombie extends MoveAside {
     /**
      * Construct a action move aside Zombie
      * Permits to a zombie to move
@@ -21,22 +21,13 @@ public class MoveAsideZombie extends AttackZombie {
     public MoveAsideZombie(Zombie z) {
         super(z);
     }
-    /**
-     * This methode tells if the zombie can do this action.
-     * It always returns true basically
-     * @return true, always
-     */
-    @Override
-    public boolean canMakeAction() {
-        return true;
-    }
 
     /**
      * Indicate to the zombie the area with the most level of noise
      * @return the location for the zombie to go
      */
     protected Location whereToGo(Cell cellToReach){ // mettre une cell en attribut
-        Cell cell = this.zombie.getCell();
+        Cell cell = this.actor.getCell();
         Position p = cell.getPosition();
         int xCell = p.getX();
         int yCell = p.getY();
@@ -105,39 +96,11 @@ public class MoveAsideZombie extends AttackZombie {
      */
     @Override
     public boolean make(Callable callable){
-        Cell cell = this.zombie.getCell();
-        Position p = cell.getPosition();
-        int xCell = p.getX();
-        int yCell = p.getY();
-        Map map = this.zombie.getGame().getMap();
         Cell cellToReach = (Cell)callable;
         Location l = whereToGo(cellToReach);
-
-        if (!canMakeAction() || whereToGo(cellToReach)==null || map.isWall(cell,l) || !map.isOpenDoor(cell,l)) {
+        if (whereToGo(cellToReach)==null) {
             return false;
         }
-        switch (l) {
-            case NORTH:
-                map.putActorONCell(this.zombie, new Position(xCell - 1, yCell));
-                System.out.println("up");
-                return true;
-            case SOUTH:
-                map.putActorONCell(this.zombie, new Position(xCell + 1, yCell));
-                System.out.println("down");
-                return true;
-            case EAST:
-                map.putActorONCell(this.zombie, new Position(xCell, yCell + 1));
-                System.out.println("east");
-                return true;
-            case WEST:
-                map.putActorONCell(this.zombie, new Position(xCell, yCell - 1));
-                System.out.println("west");
-                return true;
-
-            default:
-                System.out.println("le zombie ne peut pas bouger");
-                return false;
-
-        }
+        return super.make(l);
     }
 }
