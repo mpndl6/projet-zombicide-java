21d9bcc3c5b8f2716bc0d871dc4fc942bd7f2461
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-22 12:43:24 +0200

    modification de la méthode whereToGo : désormais un paramètre sera mit à la méthode pour savoir où aller directement modification de la méthode MoveAsideZombie: gestion des cas où c'est un mur et si la porte est fermée au tout debut du code pour éviter les répétitions inutiles

--- src/zombicide/action/actionZombie/MoveAsideZombie.java
+++ src/zombicide/action/actionZombie/MoveAsideZombie.java
@@ -31,59 +31,66 @@ public class MoveAsideZombie extends AttackZombie {
      * Indicate to the zombie the area with the most level of noise
      * @return the location for the zombie to go
      */
-    protected Location whereToGo(){
+    protected Location whereToGo(Cell cellToReach){ // mettre une cell en attribut
         Cell cell = this.zombie.getCell();
         Position p = cell.getPosition();
         int xCell = p.getX();
         int yCell = p.getY();
-        Map map = this.zombie.getGame().getMap();
 
-        Cell noisier = map.NoisierCell();
-        if(noisier.getPosition().getX()==xCell || noisier.getPosition().getX()==yCell) {
-            if (xCell - noisier.getPosition().getX() > 0)
-                return Location.NORTH;
-            if (yCell - noisier.getPosition().getY()>0)
+        if(cellToReach.getPosition().getX()==xCell && cellToReach.getPosition().getY()==yCell)
+            return null; // s'ils sont sur la même cell on arrête
+
+        if(cellToReach.getPosition().getX()==xCell) {
+            if (yCell - cellToReach.getPosition().getY()>0)
                 return Location.WEST;
-            else if (xCell - noisier.getPosition().getX()<0)
-                return Location.SOUTH;
-            else if (yCell - noisier.getPosition().getY()<0)
+            else if (yCell - cellToReach.getPosition().getY()<0)
                 return Location.EAST;
         }
-        Random random = new Random();
-        int dir =  random.nextInt(2);
-        if (xCell - noisier.getPosition().getX() >0 & yCell - noisier.getPosition().getY()>0){
-            switch (dir){
-                case 0:
-                    return Location.WEST;
-                case 1:
-                    return Location.NORTH;
+
+        else if (cellToReach.getPosition().getY()==yCell){
+            if (xCell - cellToReach.getPosition().getX() > 0)
+                return Location.NORTH;
+            else if (xCell - cellToReach.getPosition().getX()<0)
+                return Location.SOUTH;
+        }
+        else {
+            Random random = new Random();
+            int dir = random.nextInt(2);
+            if (xCell - cellToReach.getPosition().getX() > 0 & yCell - cellToReach.getPosition().getY() > 0) {
+                switch (dir) {
+                    case 0:
+                        return Location.WEST;
+                    case 1:
+                        return Location.NORTH;
+
+                }
+            }
+            if (xCell - cellToReach.getPosition().getX() < 0 & yCell - cellToReach.getPosition().getY() > 0) {
+                switch (dir) {
+                    case 0:
+                        return Location.WEST;
+                    case 1:
+                        return Location.SOUTH;
+                }
+            }
+            if (xCell - cellToReach.getPosition().getX() > 0 & yCell - cellToReach.getPosition().getY() < 0) {
+                switch (dir) {
+                    case 0:
+                        return Location.EAST;
+                    case 1:
+                        return Location.NORTH;
+                }
+            }
+            if (xCell - cellToReach.getPosition().getX() < 0 & yCell - cellToReach.getPosition().getY() < 0) {
+                switch (dir) {
+                    case 0:
+                        return Location.EAST;
+                    case 1:
+                        return Location.SOUTH;
+                }
             }
         }
-        if (xCell - noisier.getPosition().getX() <0 & yCell - noisier.getPosition().getY()>0){
-            switch (dir){
-                case 0:
-                    return Location.WEST;
-                case 1:
-                    return Location.SOUTH;
-            }
-        }
-        if (xCell - noisier.getPosition().getX() >0 & yCell - noisier.getPosition().getY()<0){
-            switch (dir){
-                case 0:
-                    return Location.EAST;
-                case 1:
-                    return Location.NORTH;
-            }
-        }
-        if (xCell - noisier.getPosition().getX() <0 & yCell - noisier.getPosition().getY()<0){
-            switch (dir){
-                case 0:
-                    return Location.EAST;
-                case 1:
-                    return Location.SOUTH;
-            }
-        }
-        return whereToGo();
+        return null;
     }
 
     /**
@@ -99,33 +106,30 @@ public class MoveAsideZombie extends AttackZombie {
         int xCell = p.getX();
         int yCell = p.getY();
         Map map = this.zombie.getGame().getMap();
+        Cell cellToReach = (Cell)callable;
+        Location l = whereToGo(cellToReach);
 
-        if (!canMakeAction()) {
-            System.out.println("Survivor is trying to go outside of the Map!");
+        if (!canMakeAction() || whereToGo(cellToReach)==null || map.isWall(cell,l) || !map.isOpenDoor(cell,l)) {
             return false;
         }
-        Location l = whereToGo();
-        switch (l){
+        switch (l) {
             case NORTH:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.zombie,new Position(xCell-1, yCell));
-                    return true;
-                }
+                map.putActorONCell(this.zombie, new Position(xCell - 1, yCell));
+                System.out.println("up");
+                return true;
             case SOUTH:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.zombie,new Position(xCell+1, yCell ));
-                    return true;
-                }
+                map.putActorONCell(this.zombie, new Position(xCell + 1, yCell));
+                System.out.println("down");
+                return true;
             case EAST:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.zombie,new Position(xCell , yCell+1));
-                    return true;
-                }
+                map.putActorONCell(this.zombie, new Position(xCell, yCell + 1));
+                System.out.println("east");
+                return true;
             case WEST:
-                if(!(map.isWall(cell,l) && map.isOpenDoor(cell,l))) {
-                    map.putActorONCell(this.zombie,new Position(xCell , yCell -1));
-                    return true;
-                }
+                map.putActorONCell(this.zombie, new Position(xCell, yCell - 1));
+                System.out.println("west");
+                return true;
+
             default:
                 System.out.println("le zombie ne peut pas bouger");
                 return false;
