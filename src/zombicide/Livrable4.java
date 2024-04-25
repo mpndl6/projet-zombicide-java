20e99e6d0a842065f6ee9e7bb97233ed1624d989
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-04-25 19:51:06 +0200

    suppression des impots inutiles

--- src/zombicide/Livrable4.java
+++ src/zombicide/Livrable4.java
@@ -1,13 +1,10 @@
 package zombicide;
 
 import grid.Grid;
-import zombicide.action.Action;
 import zombicide.action.actionZombie.ActionZombie;
-import zombicide.actor.Actor;
 import zombicide.actor.survivor.Survivor;
 import zombicide.actor.zombie.Zombie;
 import zombicide.actor.zombie.type.Abomination;
-import zombicide.actor.zombie.type.Walker;
 import zombicide.map.Map;
 import zombicide.map.TrainingMap3;
 import zombicide.map.cell.Cell;
@@ -33,13 +30,13 @@ public class Livrable4 {
         walker.setGame(game);
         game.addZombieGame(walker);
 
-        Grid grid = new Grid(trainningMap, 10);
-        trainningMap.putActorONCell(walker, new Position(4,2));
+        Grid grid = new Grid(trainningMap, 15);
+        trainningMap.putActorONCell(walker, new Position(4, 2));
 
-        Cell cellJul = trainningMap.getCell(new Position(2,4));
+        Cell cellJul = trainningMap.getCell(new Position(2, 4));
         cellJul.welcomeActor(new Survivor("jul"));
 
-        Cell cellGab = trainningMap.getCell(new Position(1,3));
+        Cell cellGab = trainningMap.getCell(new Position(1, 3));
         cellGab.welcomeActor(new Survivor("gab"));
 
 
@@ -48,10 +45,10 @@ public class Livrable4 {
 
         grid.displayGrid();
         ActionZombie moveWalker = walker.getAction(0);
-        while (!walker.getCell().equals(cellGab) & walker.makeAction(moveWalker, cellGab)){
+        while (!walker.getCell().equals(cellGab) & walker.makeAction(moveWalker, cellGab)) {
             grid.displayGrid();
         } // quand ça ne montre rien c'est parce que le zombie veut aller à droit (c'est aléatoire) sauf qu'il ne peut pas il faut re run
 
-    }
 
+    }
 }
