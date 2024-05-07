53a9a3bd8a25e4d1246c940c053da2091bae8f70
julien.miroux.etu@univ-lille.fr <julien.miroux.etu@univ-lille.fr>
2024-05-07 16:16:26 +0200

    Correction attackzombie

--- src/zombicide/action/actionZombie/AttackZombie.java
+++ src/zombicide/action/actionZombie/AttackZombie.java
@@ -41,7 +41,7 @@ public class AttackZombie implements ActionZombie {
             int size = listSurvivor.size();
             int n = (int)(Math.random() * size);
             Survivor target = listSurvivor.get(n);
-            if(target.isAlive() && target.getCell().canFight()) {
+            if(target.isAlive() && this.zombie.getCell().canFight()) {
                 target.beingAttacked();
                 System.out.println(this.zombie.getNickName() + " has attacked " + target.getNickName() + ".");
                 if (!target.isAlive()){ //après l'attaque dcp
